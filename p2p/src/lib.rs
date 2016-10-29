8fb27bf228aeb1713836a97486de42d12e665d32
Garrick Ollivander <garrickollivander@gmx.com>
2016-10-29 15:51:24 +0200

    nicer processing of message types

--- p2p/src/lib.rs
+++ p2p/src/lib.rs
@@ -24,12 +24,15 @@
 #[macro_use]
 extern crate bitflags;
 #[macro_use]
+extern crate enum_primitive;
+#[macro_use]
 extern crate grin_core as core;
 #[macro_use]
 extern crate log;
 extern crate mioco;
 extern crate rand;
 extern crate time;
+extern crate num;
 
 mod types;
 mod msg;
