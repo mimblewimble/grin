956dd6b8163bbde1c0500eb6aedbd5d8f43f5d79
Garrick Ollivander <garrickollivander@gmx.com>
2016-11-13 12:45:44 +0100

    fix file headers: Developer -> Grin Developer

--- core/src/core/hash.rs
+++ core/src/core/hash.rs
@@ -1,4 +1,4 @@
-// Copyright 2016 The Developers
+// Copyright 2016 The Grin Developers
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
