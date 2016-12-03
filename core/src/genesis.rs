de18d73832c821cc3c7afca4f4fa63f759d31133
Garrick Ollivander <garrickollivander@gmx.com>
2016-12-03 13:02:27 +0100

    Hashed of [u8] should send only data to hash function, not length + data

--- core/src/genesis.rs
+++ core/src/genesis.rs
@@ -35,8 +35,8 @@ pub fn genesis() -> core::Block {
 			},
 			cuckoo_len: DEFAULT_SIZESHIFT,
 			target: MAX_TARGET,
-			utxo_merkle:  [0; 32].hash(),
-			tx_merkle:  [0; 32].hash(),
+			utxo_merkle:  [].hash(),
+			tx_merkle:  [].hash(),
 			nonce: 0,
 			pow: core::Proof::zero(), // TODO get actual PoW solution
 		},
