c4603ed43ef5905bfc2acf753f32b0131bb98434
Taylor Rhodes <taylorsingletonfookes@live.com>
2017-04-20 19:24:24 -0700

    block encode/decode for Output

--- store/src/encoding/block.rs
+++ store/src/encoding/block.rs
@@ -6,6 +6,7 @@ use tokio_io::*;
 use bytes::{Bytes, BytesMut, BufMut};
 
 use core::core::{Input, Output, Proof, Transaction, TxKernel, Block, BlockHeader};
+use core::core::transaction::OutputFeatures;
 use secp::pedersen::{RangeProof, Commitment};
 use secp::constants::PEDERSEN_COMMITMENT_SIZE;
 
@@ -57,3 +58,37 @@ impl BlockDecode for Input {
 	}
 }
 
+impl BlockEncode for Output {
+	fn block_encode(&self, dst: &mut BytesMut) {
+		dst.put_u8(self.features.bits());
+		dst.put_slice(self.commit.as_ref());
+		dst.put_slice(self.proof.as_ref());
+	}
+}
+
+impl BlockDecode for Output {
+	fn block_decode(src: Bytes) -> io::Result<Self> {
+		let (feature_data, remaining) = src.split_at(0);
+		let (commit_data, proof_data) = remaining.split_at(PEDERSEN_COMMITMENT_SIZE);
+		if proof_data.len() == 0 {
+			return Err(io::Error::from(io::ErrorKind::InvalidData));
+		}
+		let mut c = [0; PEDERSEN_COMMITMENT_SIZE];
+		for i in 0..PEDERSEN_COMMITMENT_SIZE {
+			c[i] = commit_data[i];
+		}
+
+		let mut p = [0; 5134];
+		for i in 0..proof_data.len() {
+			p[i] = proof_data[i];
+		}
+		Ok(Output {
+		       features: OutputFeatures::from_bits(feature_data[0]).unwrap(),
+		       commit: Commitment(c),
+		       proof: RangeProof {
+		           proof: p,
+		           plen: proof_data.len(),
+		       },
+		   })
+	}
+}
