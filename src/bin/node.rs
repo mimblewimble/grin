f18aeb3024b33dded2f906b8a1bd20d83017b88f
Ignotus Peverell <igno.peverell@protonmail.com>
2017-04-24 18:55:01 -0700

    Start of a binary file for the grin node

--- src/bin/node.rs
+++ src/bin/node.rs
@@ -14,5 +14,50 @@
 
 //! Main for building the binary of a Grin peer-to-peer node.
 
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate serde;
+extern crate serde_json;
+
+extern crate grin_grin as grin;
+
+const GRIN_HOME: &'static str = ".grin";
+
+use std::env;
+use std::thread;
+use std::io::Read;
+use std::fs::File;
+use std::time::Duration;
+
 fn main() {
+	env_logger::init().unwrap();
+
+	info!("Starting the Grin server...");
+	grin::Server::start(read_config()).unwrap();
+
+	loop {
+		thread::sleep(Duration::from_secs(60));
+	}
+}
+
+fn read_config() -> grin::ServerConfig {
+	let mut config_path = env::home_dir().ok_or("Failed to detect home directory!").unwrap();
+	config_path.push(GRIN_HOME);
+	if !config_path.exists() {
+		return default_config();
+	}
+	let mut config_file = File::open(config_path).unwrap();
+	let mut config_content = String::new();
+	config_file.read_to_string(&mut config_content).unwrap();
+	serde_json::from_str(config_content.as_str()).unwrap()
+}
+
+fn default_config() -> grin::ServerConfig {
+	grin::ServerConfig {
+		cuckoo_size: 12,
+		seeding_type: grin::Seeding::WebStatic,
+		enable_mining: false,
+		..Default::default()
+	}
 }
