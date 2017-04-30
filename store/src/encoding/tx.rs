f73e292a0d74929f9a94b8db7da17e85b1748007
Jacob Payne <jacobapayne@gmail.com>
2017-04-30 00:07:13 -0700

    Copy Output + Input Block{Encode, Decode} to Tx{Encode,Decode}

--- store/src/encoding/tx.rs
+++ store/src/encoding/tx.rs
@@ -52,25 +52,60 @@ trait TxDecode: Sized {
 
 impl TxEncode for Output {
 	fn tx_encode(&self, dst: &mut BytesMut) -> Result<(), io::Error> {
-		unimplemented!()
+		dst.reserve(PEDERSEN_COMMITMENT_SIZE + 5134 + 1);
+		dst.put_u8(self.features.bits());
+		dst.put_slice(self.commit.as_ref());
+		dst.put_slice(self.proof.as_ref());
+		Ok(())
 	}
 }
 
 impl TxDecode for Output {
 	fn tx_decode(src: &mut BytesMut) -> Result<Option<Self>, io::Error> {
-		unimplemented!()
+		let output_size = PEDERSEN_COMMITMENT_SIZE + 5134 + 1;
+		if src.len() < output_size {
+			return Ok(None);
+		}
+
+		let mut buf = src.split_to(output_size).into_buf();
+		let feature_data = buf.get_u8();
+
+		let mut commit_data = [0; PEDERSEN_COMMITMENT_SIZE];
+		buf.copy_to_slice(&mut commit_data);
+
+		let mut proof_data = [0; 5134];
+		buf.copy_to_slice(&mut proof_data);
+
+		Ok(Some(Output {
+			features: OutputFeatures::from_bits(feature_data).unwrap(),
+			commit: Commitment(commit_data),
+			proof: RangeProof {
+				proof: proof_data,
+				plen: proof_data.len(),
+			},
+		}))
 	}
 }
 
 impl TxEncode for Input {
 	fn tx_encode(&self, dst: &mut BytesMut) -> Result<(), io::Error> {
-		unimplemented!()
+		dst.reserve(PEDERSEN_COMMITMENT_SIZE);
+		dst.put_slice((self.0).0.as_ref());
+		Ok(())
 	}
 }
 
 impl TxDecode for Input {
 	fn tx_decode(src: &mut BytesMut) -> Result<Option<Self>, io::Error> {
-		unimplemented!()
+		if src.len() < PEDERSEN_COMMITMENT_SIZE {
+			return Ok(None);
+		}
+
+		let mut buf = src.split_to(PEDERSEN_COMMITMENT_SIZE).into_buf();
+		let mut c = [0; PEDERSEN_COMMITMENT_SIZE];
+		buf.copy_to_slice(&mut c);
+
+		Ok(Some(Input(Commitment(c))))
 	}
 }
 
