e070b8688924dfb728940fce955df1f713c66804
yeastplume <y@y.com>
2017-07-28 13:21:39 +0100

    u64 internal difficulty representation, and integration of latest Cuckoo-miner API

--- core/src/core/target.rs
+++ core/src/core/target.rs
@@ -15,60 +15,61 @@
 //! Definition of the maximum target value a proof-of-work block hash can have
 //! and
 //! the related difficulty, defined as the maximum target divided by the hash.
+//!
+//! Note this is now wrapping a simple U64 now, but it's desirable to keep the
+//! wrapper in case the internal representation needs to change again
 
 use std::fmt;
 use std::ops::{Add, Mul, Div, Sub};
+use std::io::Cursor;
+use std::u64::MAX;
 
-use bigint::BigUint;
 use serde::{Serialize, Serializer, Deserialize, Deserializer, de};
+use byteorder::{ByteOrder, ReadBytesExt, BigEndian};
 
 use core::hash::Hash;
 use ser::{self, Reader, Writer, Writeable, Readable};
 
 /// The target is the 32-bytes hash block hashes must be lower than.
-pub const MAX_TARGET: [u8; 32] = [0xf, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                                  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                                  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
+pub const MAX_TARGET: [u8; 8] = [0xf, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
 
 /// The difficulty is defined as the maximum target divided by the block hash.
 #[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub struct Difficulty {
-	num: BigUint,
+	num: u64,
 }
 
 impl Difficulty {
 	/// Difficulty of zero, which is practically invalid (not target can be
 	/// calculated from it) but very useful as a start for additions.
 	pub fn zero() -> Difficulty {
-		Difficulty { num: BigUint::new(vec![0]) }
+		Difficulty { num: 0 }
 	}
 
 	/// Difficulty of one, which is the minumum difficulty (when the hash
 	/// equals the max target)
 	pub fn one() -> Difficulty {
-		Difficulty { num: BigUint::new(vec![1]) }
+		Difficulty { num: 1 }
 	}
 
 	/// Convert a `u32` into a `Difficulty`
-	pub fn from_num(num: u32) -> Difficulty {
-		Difficulty { num: BigUint::new(vec![num]) }
-	}
-
-	/// Convert a `BigUint` into a `Difficulty`
-	pub fn from_biguint(num: BigUint) -> Difficulty {
+	pub fn from_num(num: u64) -> Difficulty {
 		Difficulty { num: num }
 	}
 
 	/// Computes the difficulty from a hash. Divides the maximum target by the
 	/// provided hash.
 	pub fn from_hash(h: &Hash) -> Difficulty {
-		let max_target = BigUint::from_bytes_be(&MAX_TARGET);
-		let h_num = BigUint::from_bytes_be(&h[..]);
-		Difficulty { num: max_target / h_num }
+		let max_target = BigEndian::read_u64(&MAX_TARGET);
+		//Use the first 64 bits of the given hash
+		let mut in_vec=h.to_vec();
+		in_vec.truncate(8);
+		let num = BigEndian::read_u64(&in_vec);
+		Difficulty { num: max_target / num }
 	}
 
-	/// Converts the difficulty into a bignum
-	pub fn into_biguint(self) -> BigUint {
+	/// Converts the difficulty into a u64
+	pub fn into_num(&self) -> u64 {
 		self.num
 	}
 }
@@ -109,17 +110,14 @@ impl Div<Difficulty> for Difficulty {
 
 impl Writeable for Difficulty {
 	fn write<W: Writer>(&self, writer: &mut W) -> Result<(), ser::Error> {
-		let data = self.num.to_bytes_be();
-		try!(writer.write_u8(data.len() as u8));
-		writer.write_fixed_bytes(&data)
+		writer.write_u64(self.num)
 	}
 }
 
 impl Readable for Difficulty {
 	fn read(reader: &mut Reader) -> Result<Difficulty, ser::Error> {
-		let dlen = try!(reader.read_u8());
-		let data = try!(reader.read_fixed_bytes(dlen as usize));
-		Ok(Difficulty { num: BigUint::from_bytes_be(&data[..]) })
+		let data = try!(reader.read_u64());
+		Ok(Difficulty { num: data })
 	}
 }
 
@@ -127,7 +125,7 @@ impl Serialize for Difficulty {
 	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
 		where S: Serializer
 	{
-		serializer.serialize_str(self.num.to_str_radix(10).as_str())
+		serializer.serialize_u64(self.num)
 	}
 }
 
@@ -135,7 +133,7 @@ impl<'de> Deserialize<'de> for Difficulty {
 	fn deserialize<D>(deserializer: D) -> Result<Difficulty, D::Error>
 		where D: Deserializer<'de>
 	{
-		deserializer.deserialize_i32(DiffVisitor)
+		deserializer.deserialize_u64(DiffVisitor)
 	}
 }
 
@@ -151,9 +149,10 @@ impl<'de> de::Visitor<'de> for DiffVisitor {
 	fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
 		where E: de::Error
 	{
-		let bigui = BigUint::parse_bytes(s.as_bytes(), 10).ok_or_else(|| {
-        de::Error::invalid_value(de::Unexpected::Str(s), &"a value number")
-      })?;
-		Ok(Difficulty { num: bigui })
+		let num_in = s.parse::<u64>();
+		if let Err(e)=num_in {
+        	return Err(de::Error::invalid_value(de::Unexpected::Str(s), &"a value number"));
+      	};
+		Ok(Difficulty { num: num_in.unwrap() })
 	}
 }
