c557e2ceef24301c2d55a72f2e6687ded2cb403e
antiochp <30642645+antiochp@users.noreply.github.com>
2017-09-27 18:10:49 -0400

    introduce grin_keychain encapsulate derivation of secret_keys

---
+++ keychain/src/lib.rs
@@ -0,0 +1,26 @@
+// Copyright 2016 The Grin Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Library module for the key holder functionalities provided by Grin.
+
+extern crate blake2_rfc as blake2;
+extern crate byteorder;
+extern crate secp256k1zkp as secp;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+mod extkey;
+pub mod keychain;
+pub use keychain::Keychain;
