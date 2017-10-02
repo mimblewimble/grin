2849aad23350dd286f742983fa947bfe6abc0f95
antiochp <30642645+antiochp@users.noreply.github.com>
2017-10-02 15:28:36 -0400

    basic test around key derivation

--- keychain/src/keychain.rs
+++ keychain/src/keychain.rs
@@ -76,7 +76,7 @@ impl Keychain {
 	// TODO - this is a work in progress
 	// TODO - smarter lookups - can we cache key_id/fingerprint -> derivation number somehow?
 	fn derived_key(&self, pubkey: &Identifier) -> Result<SecretKey, Error> {
-		for i in 1..1000 {
+		for i in 1..10000 {
 			let extkey = self.extkey.derive(&self.secp, i)?;
 			if extkey.identifier() == *pubkey {
 				return Ok(extkey.key)
@@ -154,7 +154,33 @@ impl Keychain {
 		Ok(sig)
 	}
 
-	 pub fn secp(&self) -> &Secp256k1 {
-		 &self.secp
-	 }
+	pub fn secp(&self) -> &Secp256k1 {
+		&self.secp
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use keychain::Keychain;
+	use secp;
+
+	#[test]
+	fn test_key_derivation() {
+		let secp = secp::Secp256k1::with_caps(secp::ContextFlag::Commit);
+
+		let keychain = Keychain::from_random_seed().unwrap();
+
+		// use the keychain to derive a "pubkey" based on the underlying seed
+		let pubkey = keychain.derive_pubkey(1).unwrap();
+
+		let msg_bytes = [0; 32];
+		let msg = secp::Message::from_slice(&msg_bytes[..]).unwrap();
+
+		// now create a zero commitment using the key on the keychain associated with the pubkey
+		let commit = keychain.commit(0, &pubkey).unwrap();
+
+		// now check we can use our key to verify a signature from this zero commitment
+		let sig = keychain.sign(&msg, &pubkey).unwrap();
+		secp.verify_from_commit(&msg, &sig, &commit).unwrap();
+	}
 }
