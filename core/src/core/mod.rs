e40d56affe628b2dc38749209db9c61e1cab75b2
Yeastplume <yeastplume@gmail.com>
2017-11-07 16:37:03 +0000

    rustfmt

--- core/src/core/mod.rs
+++ core/src/core/mod.rs
@@ -55,7 +55,7 @@ pub trait Committed {
 		let mut output_commits = map_vec!(self.outputs_committed(), |out| out.commitment());
 
 		// add the overage as output commitment if positive, as an input commitment if
-  // negative
+		// negative
 		let overage = self.overage();
 		if overage != 0 {
 			let over_commit = secp.commit_value(overage.abs() as u64).unwrap();
@@ -187,9 +187,9 @@ impl Writeable for Proof {
 /// Common method for parsing an amount from human-readable, and converting
 /// to internally-compatible u64
 
-pub fn amount_from_hr_string(amount:&str) -> Result<u64, ParseFloatError> {
+pub fn amount_from_hr_string(amount: &str) -> Result<u64, ParseFloatError> {
 	let amount = amount.parse::<f64>()?;
-	Ok((amount*GRIN_BASE as f64) as u64)
+	Ok((amount * GRIN_BASE as f64) as u64)
 }
 
 /// Common method for converting an amount to a human-readable string
@@ -214,19 +214,21 @@ mod test {
 	pub fn test_amount_to_hr() {
 		assert!(50123456789 == amount_from_hr_string("50.123456789").unwrap());
 		assert!(50 == amount_from_hr_string(".000000050").unwrap());
-		assert!(1 ==  amount_from_hr_string(".000000001").unwrap());
-		assert!(0 ==  amount_from_hr_string(".0000000009").unwrap());
-		assert!(500_000_000_000 ==  amount_from_hr_string("500").unwrap());
-		assert!(5_000_000_000_000_000_000 ==  amount_from_hr_string("5000000000.00000000000").unwrap());
+		assert!(1 == amount_from_hr_string(".000000001").unwrap());
+		assert!(0 == amount_from_hr_string(".0000000009").unwrap());
+		assert!(500_000_000_000 == amount_from_hr_string("500").unwrap());
+		assert!(
+			5_000_000_000_000_000_000 == amount_from_hr_string("5000000000.00000000000").unwrap()
+		);
 	}
 
 	#[test]
 	pub fn test_hr_to_amount() {
 		assert!("50.123456789" == amount_to_hr_string(50123456789));
-		assert!( "0.000000050" == amount_to_hr_string(50));
-		assert!( "0.000000001" ==  amount_to_hr_string(1));
-		assert!("500.000000000" ==  amount_to_hr_string(500_000_000_000));
-		assert!("5000000000.000000000"  ==  amount_to_hr_string(5_000_000_000_000_000_000));
+		assert!("0.000000050" == amount_to_hr_string(50));
+		assert!("0.000000001" == amount_to_hr_string(1));
+		assert!("500.000000000" == amount_to_hr_string(500_000_000_000));
+		assert!("5000000000.000000000" == amount_to_hr_string(5_000_000_000_000_000_000));
 	}
 
 	#[test]
@@ -345,11 +347,11 @@ mod test {
 
 		{
 			// Alice gets 2 of her pre-existing outputs to send 5 coins to Bob, they
-   // become inputs in the new transaction
+			// become inputs in the new transaction
 			let (in1, in2) = (input(4, key_id1), input(3, key_id2));
 
 			// Alice builds her transaction, with change, which also produces the sum
-   // of blinding factors before they're obscured.
+			// of blinding factors before they're obscured.
 			let (tx, sum) =
 				build::transaction(vec![in1, in2, output(1, key_id3), with_fee(2)], &keychain)
 					.unwrap();
@@ -358,8 +360,8 @@ mod test {
 		}
 
 		// From now on, Bob only has the obscured transaction and the sum of
-  // blinding factors. He adds his output, finalizes the transaction so it's
-  // ready for broadcast.
+		// blinding factors. He adds his output, finalizes the transaction so it's
+		// ready for broadcast.
 		let (tx_final, _) = build::transaction(
 			vec![
 				initial_tx(tx_alice),
@@ -419,7 +421,7 @@ mod test {
 		let key_id3 = keychain.derive_key_id(3).unwrap();
 
 		// first check we can add a timelocked tx where lock height matches current block height
-  // and that the resulting block is valid
+		// and that the resulting block is valid
 		let tx1 = build::transaction(
 			vec![
 				input(5, key_id1.clone()),
@@ -458,9 +460,7 @@ mod test {
 			&key_id3.clone(),
 		).unwrap();
 		match b.validate(keychain.secp()) {
-			Err(KernelLockHeight {
-				lock_height: height,
-			}) => {
+			Err(KernelLockHeight { lock_height: height }) => {
 				assert_eq!(height, 2);
 			}
 			_ => panic!("expecting KernelLockHeight error here"),
