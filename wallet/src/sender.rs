94f93ff38f94ab4502da7412b31697a392855d78
Simon B <simon.bohlin@gmail.com>
2017-12-10 15:48:53 +0100

    distinguish select (among futures) from select coins. Regex search in project for select\b shows we hardly use select, but maybe could use it to add timeouts more cleanly. ("Want to add a timeout to any future? Just do a select of that future and a timeout future!" from https://aturon.github.io/blog/2016/08/11/futures/)

--- wallet/src/sender.rs
+++ wallet/src/sender.rs
@@ -115,7 +115,7 @@ fn build_send_tx(
 
 	// select some spendable coins from the wallet
 	let coins = WalletData::read_wallet(&config.data_file_dir, |wallet_data| {
-		wallet_data.select(
+		wallet_data.select_coins(
 			key_id.clone(),
 			amount,
 			current_height,
@@ -155,7 +155,7 @@ pub fn issue_burn_tx(
 
 	// select some spendable coins from the wallet
 	let coins = WalletData::read_wallet(&config.data_file_dir, |wallet_data| {
-		wallet_data.select(
+		wallet_data.select_coins(
 			key_id.clone(),
 			amount,
 			current_height,
