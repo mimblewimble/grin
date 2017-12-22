c5104c39796739c430f7bede15cdd12e4a26f50f
Quentin Le Sceller <q.lesceller@gmail.com>
2017-12-22 11:18:26 -0500

    Display current chain height

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -15,7 +15,10 @@
 extern crate grin_p2p as p2p;
 extern crate term;
 
-pub fn show_status() {
+use api;
+use grin::ServerConfig;
+
+pub fn show_status(config: &ServerConfig) {
     println!();
     let title=format!("Grin Server Status ");
     let mut t = term::stdout().unwrap();
@@ -26,6 +29,22 @@ pub fn show_status() {
     t.reset().unwrap();
     writeln!(e, "Protocol version: {}", p2p::msg::PROTOCOL_VERSION).unwrap();
     writeln!(e, "User agent: {}", p2p::msg::USER_AGENT).unwrap();
+    match get_tip_from_node(config) {
+        Ok(tip) => writeln!(e, "Chain height: {}", tip.height).unwrap(),
+        Err(_) => writeln!(e, "WARNING: Client failed to get data. Is your `grin server` offline or broken?").unwrap()
+    };
     e.reset().unwrap();
     println!();
+}
+
+fn get_tip_from_node(config: &ServerConfig) -> Result<api::Tip, Error> {
+	let url = format!("http://{}/v1/chain", config.api_http_addr);
+	api::client::get::<api::Tip>(url.as_str()).map_err(|e| Error::API(e))
+}
+
+/// Error type wrapping underlying module errors.
+#[derive(Debug)]
+enum Error {
+	/// Error originating from HTTP API calls.
+	API(api::Error)
 }
