01fdca97c61d08639f3189229fa5b822451c25c9
Alexey Miroshkin <alexey.miroshkin@optiopay.com>
2018-03-15 13:30:08 +0100

    Add pattern generator and block test

--- core/fuzz/fuzz_targets/block_read.rs
+++ core/fuzz/fuzz_targets/block_read.rs
@@ -6,9 +6,10 @@ use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
 use grin_core::ser;
-use grin_core::core::{self, transaction};
+use grin_core::core::{self, block};
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<transaction::Transaction, ser::Error> = ser::deserialize(&mut d);
+	let _t: Result<block::Block, ser::Error> = ser::deserialize(&mut d);
 });
+
