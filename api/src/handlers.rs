4cd7163f0db6e74be8202d30e7181b04919a80aa
Quentin Le Sceller <q.lesceller@gmail.com>
2018-03-19 19:42:07 -0400

    Rustfmt

--- api/src/handlers.rs
+++ api/src/handlers.rs
@@ -558,7 +558,10 @@ where
 
 		// Will not do a stem transaction if our dandelion peer relay is empty
 		if !fluff && w(&self.peers).get_dandelion_relay().is_empty() {
-			debug!(LOGGER, "Missing Dandelion relay: will push stem transaction normally");
+			debug!(
+				LOGGER,
+				"Missing Dandelion relay: will push stem transaction normally"
+			);
 			fluff = true;
 		}
 
