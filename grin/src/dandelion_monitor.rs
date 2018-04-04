c4ec496f837cf660d4f102ce28f982e4ed34df08
Quentin Le Sceller <q.lesceller@gmail.com>
2018-04-04 11:56:02 -0400

    Randomize Dandelion timer

--- grin/src/dandelion_monitor.rs
+++ grin/src/dandelion_monitor.rs
@@ -51,7 +51,6 @@ pub fn monitor_transactions<T>(
 					let time_transaction = time_stem_transactions.get(tx_hash).unwrap();
 					let interval = now_utc().to_timespec().sec - time_transaction;
 
-					// TODO Randomize between 30 and 60 seconds
 					if interval >= config.dandelion_embargo {
 						let source = TxSource {
 							debug_name: "dandelion-monitor".to_string(),
