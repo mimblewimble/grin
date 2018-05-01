5b228d2ec2c092174fd78513d5f619aa6899b444
Quentin Le Sceller <q.lesceller@gmail.com>
2018-05-01 15:43:26 -0400

    Rustfmt

--- servers/src/grin/dandelion_monitor.rs
+++ servers/src/grin/dandelion_monitor.rs
@@ -82,41 +82,46 @@ pub fn monitor_transactions<T>(
 		});
 }
 
-// The role of this thread is to broadcast an aggregated transaction made of all the
-// transactions in the stempool every dandelion_patience seconds.
-// This is the only way a stem transaction can be broadcasted to the network as stem transaction
+// The role of this thread is to broadcast an aggregated transaction made of
+// all the transactions in the stempool every dandelion_patience seconds.
+// This is the only way a stem transaction can be broadcasted to the network as
+// stem transaction
 pub fn transactions_aggregator<T>(
 	config: PoolConfig,
 	tx_pool: Arc<RwLock<TransactionPool<T>>>,
 	adapter: Arc<PoolToNetAdapter>,
 	stop: Arc<AtomicBool>,
 ) where
-T: BlockChain + Send + Sync + 'static,
+	T: BlockChain + Send + Sync + 'static,
 {
 	debug!(LOGGER, "Started Dandelion Aggregator");
 	let _ = thread::Builder::new()
-	.name("dandelion-agggregator".to_string())
-	.spawn(move || {
-		loop {
-			// Broadcast the stem txs as one giant tx
-			let tx_pool = tx_pool.clone();
-			let stem_transactions = tx_pool.read().unwrap().stem_transactions.clone();
-			let stem_transactions_vec: Vec<Transaction> = stem_transactions.iter().map(|(_, tx)| *tx.clone()).collect();
-			// Take all the stem transaction and make a big transaction
-			// The multi kernel stem transaction
-			let mk_stem_transaction = transaction::aggregate_with_cut_through(stem_transactions_vec);
+		.name("dandelion-agggregator".to_string())
+		.spawn(move || {
+			loop {
+				// Broadcast the stem txs as one giant tx
+				let tx_pool = tx_pool.clone();
+				let stem_transactions = tx_pool.read().unwrap().stem_transactions.clone();
+				let stem_transactions_vec: Vec<Transaction> = stem_transactions
+					.iter()
+					.map(|(_, tx)| *tx.clone())
+					.collect();
+				// Take all the stem transaction and make a big transaction
+				// The multi kernel stem transaction
+				let mk_stem_transaction =
+					transaction::aggregate_with_cut_through(stem_transactions_vec);
 
-			match mk_stem_transaction {
-				Ok(mk_tx) => adapter.stem_tx_accepted(&mk_tx),
-				Err(e) => error!(LOGGER, "Aggregator error - {:?}", e),
-			};
+				match mk_stem_transaction {
+					Ok(mk_tx) => adapter.stem_tx_accepted(&mk_tx),
+					Err(e) => error!(LOGGER, "Aggregator error - {:?}", e),
+				};
 
-			// Broadcast to the network using the adapter
-			thread::sleep(Duration::from_secs(config.dandelion_patience as u64));
-			//stem_tx_accepted
-			if stop.load(Ordering::Relaxed) {
-				break;
+				// Broadcast to the network using the adapter
+				thread::sleep(Duration::from_secs(config.dandelion_patience as u64));
+				//stem_tx_accepted
+				if stop.load(Ordering::Relaxed) {
+					break;
+				}
 			}
-		}
-	});
+		});
 }
