ead580d4fa60581ae37039bd4873134b17879083
yeastplume <yeastplume@gmail.com>
2018-05-30 17:00:07 +0100

    re-ignore wallet integration test

--- servers/tests/wallet.rs
+++ servers/tests/wallet.rs
@@ -35,7 +35,7 @@ use util::LOGGER;
 
 /// Start 1 node mining and two wallets, then send a few
 /// transactions from one to the other
-//#[ignore]
+#[ignore]
 #[test]
 fn basic_wallet_transactions() {
 	let test_name_dir = "test_servers";
