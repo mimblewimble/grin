1c0b8e8ef28c5838998a7bf579c586ae5c1db834
Gary Yu <gairy.yu@gmail.com>
2018-07-30 12:05:22 +0800

    improve: complete the switching from deprecated time crate to chrono crate, for all the tests part

--- chain/tests/mine_simple_chain.rs
+++ chain/tests/mine_simple_chain.rs
@@ -19,7 +19,7 @@ extern crate grin_store as store;
 extern crate grin_util as util;
 extern crate grin_wallet as wallet;
 extern crate rand;
-extern crate time;
+extern crate chrono;
 
 use std::fs;
 use std::sync::Arc;
