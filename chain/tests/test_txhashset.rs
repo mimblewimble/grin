79a26f61e2fa419fa355f4f69c490e9f5989a9c0
Quentin Le Sceller <q.lesceller@gmail.com>
2018-08-01 12:33:53 -0400

    Fix Travis Test error

--- chain/tests/test_txhashset.rs
+++ chain/tests/test_txhashset.rs
@@ -19,6 +19,8 @@ extern crate grin_store as store;
 extern crate grin_wallet as wallet;
 extern crate grin_util as util;
 
+use std::collections::HashSet;
+use std::iter::FromIterator;
 use std::fs::{self, File, OpenOptions};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
@@ -158,14 +160,14 @@ fn test_unexpected_zip() {
 	assert!(txhashset::zip_read(db_root.clone(), &BlockHeader::default()).is_ok());
 	// Check that the temp dir dos not contains the strange files
 	let txhashset_zip_path = Path::new(&db_root).join("txhashset_zip");
-	assert!(txhashset_contains_expected_files(txhashset_zip_path.clone()));
+	assert!(txhashset_contains_expected_files("txhashset_zip".to_string(), txhashset_zip_path.clone()));
 	fs::remove_dir_all(Path::new(&db_root).join("txhashset_zip")).unwrap();
 
 	let zip_file = File::open(zip_path).unwrap();
 	assert!(txhashset::zip_write(db_root.clone(), zip_file, &BlockHeader::default()).is_ok());
 	// Check that the txhashset dir dos not contains the strange files
-	let txhashset_zip_path = Path::new(&db_root).join("txhashset");
-	assert!(txhashset_contains_expected_files(txhashset_zip_path.clone()));
+	let txhashset_path = Path::new(&db_root).join("txhashset");
+	assert!(txhashset_contains_expected_files("txhashset".to_string(), txhashset_path.clone()));
 	fs::remove_dir_all(Path::new(&db_root).join("txhashset")).unwrap();
 }
 
@@ -190,13 +192,15 @@ fn write_file (db_root: String) {
 		.open(Path::new(&db_root).join("txhashset").join("strange_dir").join("strange_subdir").join("strange3")).unwrap();
 }
 
-fn txhashset_contains_expected_files(path_buf: PathBuf) -> bool {
+fn txhashset_contains_expected_files(dirname: String, path_buf: PathBuf) -> bool {
 	let list_zip_files = file::list_files(path_buf.into_os_string().into_string().unwrap());
-	println!("{:?}", list_zip_files);
-	if list_zip_files[1] == "output" && list_zip_files[2] == "pmmr_hash.bin" && list_zip_files[3] == "pmmr_data.bin" && list_zip_files[4] == "rangeproof" && list_zip_files[5] == "pmmr_hash.bin" && list_zip_files[6] == "pmmr_data.bin" && list_zip_files[7] == "kernel" && list_zip_files[8] == "pmmr_hash.bin" && list_zip_files[9] == "pmmr_data.bin" {
-		true
-	} else {
-		false
-	}
-	
+	let zip_files_hashset: HashSet<_> = HashSet::from_iter(list_zip_files.iter().cloned());
+	let expected_files = vec![dirname, "output".to_string(), "rangeproof".to_string(), "kernel".to_string(), "pmmr_hash.bin".to_string(), "pmmr_data.bin".to_string()];
+	let expected_files_hashset = HashSet::from_iter(expected_files.iter().cloned());
+	let intersection: HashSet<_>  = zip_files_hashset.difference(&expected_files_hashset).collect();
+	if intersection.is_empty() {
+	 true
+ 	}	else {
+	 false
+	 }
 }
