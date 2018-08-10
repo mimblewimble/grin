5ab6411161769171781890ef14237764e88e70a2
antiochp <30642645+antiochp@users.noreply.github.com>
2018-08-10 14:56:35 +0100

    run rustfmt against chain crate suspect it has not been running successfully for a while...

--- chain/src/lib.rs
+++ chain/src/lib.rs
@@ -31,8 +31,8 @@ extern crate serde;
 extern crate serde_derive;
 #[macro_use]
 extern crate slog;
-extern crate failure;
 extern crate chrono;
+extern crate failure;
 #[macro_use]
 extern crate failure_derive;
 
