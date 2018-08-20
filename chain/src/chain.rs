e1ac2dc09cfbb5d273566c1ef59aa0c6c86b2dc4
antiochp <30642645+antiochp@users.noreply.github.com>
2018-08-20 20:43:28 +0100

    rustfmt

--- chain/src/chain.rs
+++ chain/src/chain.rs
@@ -25,9 +25,7 @@ use lmdb;
 use core::core::hash::{Hash, Hashed};
 use core::core::merkle_proof::MerkleProof;
 use core::core::target::Difficulty;
-use core::core::{
-	Block, BlockHeader, Output, OutputIdentifier, Transaction, TxKernel,
-};
+use core::core::{Block, BlockHeader, Output, OutputIdentifier, Transaction, TxKernel};
 use core::global;
 use error::{Error, ErrorKind};
 use grin_store::Error::NotFoundErr;
