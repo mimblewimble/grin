00f1ed4d7f98069bc1699b74514060b59a0d67b9
yeastplume <yeastplume@gmail.com>
2018-08-29 16:43:03 +0100

    rustfmt

--- src/bin/cmd/wallet.rs
+++ src/bin/cmd/wallet.rs
@@ -12,12 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::path::PathBuf;
 /// Wallet commands processing
 use std::process::exit;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use std::path::PathBuf;
 
 use clap::ArgMatches;
 
