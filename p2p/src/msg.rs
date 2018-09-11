eb9f7e82a5c7327203fc8347b20e58569b8b2425
Gary Yu <gairy.yu@gmail.com>
2018-09-11 18:41:51 +0800

    refactor according to review comments

--- p2p/src/msg.rs
+++ p2p/src/msg.rs
@@ -118,18 +118,14 @@ pub fn read_exact(
 	let sleep_time = time::Duration::from_micros(10);
 	let mut count = time::Duration::new(0, 0);
 
-	let exact = buf.len();
 	let mut read = 0;
 	loop {
 		match conn.read(buf) {
 			Ok(0) => {
-				if read < exact {
-					return Err(io::Error::new(
-						io::ErrorKind::ConnectionAborted,
-						"read_exact",
-					));
-				}
-				break;
+				return Err(io::Error::new(
+					io::ErrorKind::ConnectionAborted,
+					"read_exact",
+				));
 			}
 			Ok(n) => {
 				let tmp = buf;
