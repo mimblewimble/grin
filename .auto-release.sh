b81de806f8284379e58beda6f3db169b73d369ce
Gary Yu <gairy.yu@gmail.com>
2018-09-19 20:37:08 +0800

    fix: '#' in file name will be converted as '.', replace '#' with '-'

--- .auto-release.sh
+++ .auto-release.sh
@@ -5,12 +5,12 @@ tagname=`git describe --tags --exact-match 2>/dev/null || git symbolic-ref -q --
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     # Do some custom requirements on OS X
-    cd target/release ; rm -f *.tgz; tar zcf "grin-$tagname#$TRAVIS_JOB_ID-osx.tgz" grin
+    cd target/release ; rm -f *.tgz; tar zcf "grin-$tagname-$TRAVIS_JOB_ID-osx.tgz" grin
     /bin/ls -ls *.tgz  | awk '{print $6,$7,$8,$9,$10}'
     cd - > /dev/null;
 else
     # Do some custom requirements on Linux
-    cd target/release ; rm -f *.tgz; tar zcf "grin-$tagname#$TRAVIS_JOB_ID-linux-amd64.tgz" grin
+    cd target/release ; rm -f *.tgz; tar zcf "grin-$tagname-$TRAVIS_JOB_ID-linux-amd64.tgz" grin
     /bin/ls -ls *.tgz  | awk '{print $6,$7,$8,$9,$10}'
     cd - > /dev/null;
 fi
