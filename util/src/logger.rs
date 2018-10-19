fb445bce8d0fa46d1cef6d547094498fc41aedba
Eugene P <eupn@protonmail.com>
2018-10-20 02:17:22 +0300

    Support different logging levels for file and stdout

--- util/src/logger.rs
+++ util/src/logger.rs
@@ -29,8 +29,9 @@ use log4rs::append::rolling_file::{
 	policy::compound::trigger::size::SizeTrigger, policy::compound::CompoundPolicy,
 	RollingFileAppender,
 };
+use log4rs::filter::threshold::ThresholdFilter;
 use log4rs::append::Append;
-use log4rs::config::{Appender, Config, Logger, Root};
+use log4rs::config::{Appender, Config, Root};
 use log4rs::encode::pattern::PatternEncoder;
 
 fn convert_log_level(in_level: &LogLevel) -> LevelFilter {
@@ -59,71 +60,72 @@ pub fn init_logger(config: Option<LoggingConfig>) {
 	if let Some(c) = config {
 		let level_stdout = convert_log_level(&c.stdout_log_level);
 		let level_file = convert_log_level(&c.file_log_level);
+		let level_minimum;
+
+		// Determine minimum logging level for Root logger
+		if level_stdout > level_file {
+			level_minimum = level_stdout;
+		} else {
+			level_minimum = level_file;
+		}
 
 		// Start logger
 		let stdout = ConsoleAppender::builder()
 			.encoder(Box::new(PatternEncoder::default()))
 			.build();
 
-		// If maximum log size is specified, use rolling file appender
-		// or use basic one otherwise
-		let file: Box<Append> = {
-			if let Some(size) = c.log_max_size {
-				let roller = FixedWindowRoller::builder()
-					.build("grin_{}.gz", 32)
-					.unwrap();
-				let trigger = SizeTrigger::new(size);
-
-				let policy = CompoundPolicy::new(Box::new(trigger), Box::new(roller));
-
-				Box::new(
-					RollingFileAppender::builder()
-						.encoder(Box::new(PatternEncoder::new("{d} {M} {l} - {m}{n}")))
-						.build(c.log_file_path, Box::new(policy))
-						.unwrap(),
-				)
-			} else {
-				Box::new(
-					FileAppender::builder()
-						.encoder(Box::new(PatternEncoder::new("{d} {M} {l} - {m}{n}")))
-						.build(c.log_file_path)
-						.unwrap(),
-				)
-			}
-		};
-
-		let mut config = Config::builder();
 		let mut root = Root::builder();
 
-		if c.log_to_stdout {
-			config = config
-				.appender(Appender::builder().build("stdout", Box::new(stdout)))
-				.logger(
-					Logger::builder()
-						.additive(true)
-						.appender("stdout")
-						.build("stdout", level_stdout),
-				);
+		let mut appenders = vec![];
 
-			root = root.appender("stdout");
+		if c.log_to_stdout {
+			let filter = Box::new(ThresholdFilter::new(level_stdout));
+			appenders.push(Appender::builder().filter(filter).build("stdout", Box::new(stdout)));
 		}
 
 		if c.log_to_file {
-			config = config
-				.appender(Appender::builder().build("file", file))
-				.logger(
-					Logger::builder()
-						.additive(true)
-						.appender("file")
-						.build("file", level_file),
-				);
+			// If maximum log size is specified, use rolling file appender
+			// or use basic one otherwise
+			let filter = Box::new(ThresholdFilter::new(level_file));
+			let file: Box<Append> = {
+				if let Some(size) = c.log_max_size {
+					let roller = FixedWindowRoller::builder()
+						.build("grin_{}.gz", 32)
+						.unwrap();
+					let trigger = SizeTrigger::new(size);
+
+					let policy = CompoundPolicy::new(Box::new(trigger), Box::new(roller));
+
+					Box::new(
+						RollingFileAppender::builder()
+							.append(c.log_file_append)
+							.encoder(Box::new(PatternEncoder::new("{d} {l} {M} - {m}{n}")))
+							.build(c.log_file_path, Box::new(policy))
+							.unwrap(),
+					)
+				} else {
+					Box::new(
+						FileAppender::builder()
+							.append(c.log_file_append)
+							.encoder(Box::new(PatternEncoder::new("{d} {l} {M} - {m}{n}")))
+							.build(c.log_file_path)
+							.unwrap(),
+					)
+				}
+			};
+
+			appenders.push(Appender::builder().filter(filter).build("file", file));
 		}
 
-		let config = config.build(root.build(LevelFilter::Off)).unwrap();
+		let config = Config::builder()
+			.appenders(appenders)
+			.build(root.appender("stdout").appender("file").build(level_minimum))
+			.unwrap();
 
 		let _ = log4rs::init_config(config).unwrap();
 
-		debug!("log4rs is initialized");
+		warn!("log4rs is initialized, file level: {:?}, stdout level: {:?}, min. level: {:?}",
+			  level_file, level_stdout, level_minimum);
 
 		// Logger configuration successfully injected into LOGGING_CONFIG...
 		let mut was_init_ref = WAS_INIT.lock();
