800e8939ac9ff8399273c4f01f9de9c46350e1da
antiochp <30642645+antiochp@users.noreply.github.com>
2018-10-29 11:14:22 +0000

    we do want to run compaction for archive nodes (from the adapter)

--- servers/src/common/adapters.rs
+++ servers/src/common/adapters.rs
@@ -515,8 +515,8 @@ impl NetToChainAdapter {
 	}
 
 	fn check_compact(&self) {
-		// no compaction during sync or if we're in historical mode
-		if self.archive_mode || self.sync_state.is_syncing() {
+		// Skip compaction if we are syncing.
+		if self.sync_state.is_syncing() {
 			return;
 		}
 
