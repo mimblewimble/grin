1f53b209b239b5d683adfb66a0b897d53208a11c
Ignotus Peverell <igno.peverell@protonmail.com>
2018-11-18 04:05:12 +0000

    Cuckaroo validator, still needs to be tested with vectors from the @tromp implementation

--- core/src/pow/cuckaroo.rs
+++ core/src/pow/cuckaroo.rs
@@ -23,11 +23,10 @@
 //! In Cuckaroo, edges are calculated by repeatedly hashing the seeds to
 //! obtain blocks of values. Nodes are then extracted from those edges.
 
-use pow::common::{CuckooParams, Edge, EdgeType};
+use pow::common::{CuckooParams, EdgeType};
 use pow::error::{Error, ErrorKind};
+use pow::siphash::siphash_block;
 use pow::{PoWContext, Proof};
-
-use std::cmp;
 
 /// Cuckatoo cycle context. Only includes the verifier for now.
 pub struct CuckarooContext<T>
@@ -41,10 +40,9 @@ impl<T> PoWContext<T> for CuckarooContext<T>
 where
 	T: EdgeType,
 {
-	fn new(edge_bits: u8, proof_size: usize, max_sols: u32) -> Result<Box<Self>, Error> {
+	fn new(edge_bits: u8, proof_size: usize, _max_sols: u32) -> Result<Box<Self>, Error> {
 		let params = CuckooParams::new(edge_bits, proof_size)?;
-		let num_edges = to_edge!(params.num_edges);
-		Ok(Box::new(CuckarooContext { params }))
+		Ok(Box::new(CuckarooContext{params}))
 	}
 
 	fn set_header_nonce(
@@ -61,6 +59,63 @@ where
 	}
 
 	fn verify(&self, proof: &Proof) -> Result<(), Error> {
-		unimplemented!()
+		let nonces = &proof.nonces;
+		let mut uvs = vec![0u64; 2 * proof.proof_size()];
+		let mut xor0: u64 = 0;
+		let mut xor1: u64 = 0;
+
+		for n in 0..proof.proof_size() {
+			if nonces[n] > to_u64!(self.params.edge_mask) {
+				return Err(ErrorKind::Verification("edge too big".to_owned()))?;
+			}
+			if n > 0 && nonces[n] <= nonces[n - 1] {
+				return Err(ErrorKind::Verification("edges not ascending".to_owned()))?;
+			}
+			let edge = to_edge!(siphash_block(&self.params.siphash_keys, n as u64));
+			uvs[2 * n] = to_u64!(edge & self.params.edge_mask);
+			uvs[2 * n + 1] = to_u64!((edge >> 32) & self.params.edge_mask);
+			xor0 ^= uvs[2 * n];
+			xor1 ^= uvs[2 * n + 1];
+		}
+		if xor0 | xor1 != 0 {
+			return Err(ErrorKind::Verification(
+				"endpoints don't match up".to_owned(),
+			))?;
+		}
+		let mut n = 0;
+		let mut i = 0;
+		let mut j;
+		loop {
+			// follow cycle
+			j = i;
+			let mut k = j;
+			loop {
+				k = (k + 2) % (2 * self.params.proof_size);
+				if k == i {
+					break;
+				}
+				if uvs[k] == uvs[i] {
+					// find other edge endpoint matching one at i
+					if j != i {
+						return Err(ErrorKind::Verification("branch in cycle".to_owned()))?;
+					}
+					j = k;
+				}
+			}
+			if j == i {
+				return Err(ErrorKind::Verification("cycle dead ends".to_owned()))?;
+			}
+			i = j ^ 1;
+			n += 1;
+			if i == 0 {
+				break;
+			}
+		}
+		if n == self.params.proof_size {
+			Ok(())
+		} else {
+			Err(ErrorKind::Verification("cycle too short".to_owned()))?
+		}
 	}
 }
+
