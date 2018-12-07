49782be3ecbde21315565cb4c9ae87473a9b28d9
Ignotus Peverell <igno.peverell@protonmail.com>
2018-12-07 04:35:53 +0000

    Tests in chain and pool crates don't depend on wallet anymore

--- chain/tests/mine_simple_chain.rs
+++ chain/tests/mine_simple_chain.rs
@@ -18,7 +18,6 @@ extern crate grin_core as core;
 extern crate grin_keychain as keychain;
 extern crate grin_store as store;
 extern crate grin_util as util;
-extern crate grin_wallet as wallet;
 extern crate rand;
 
 use chrono::Duration;
