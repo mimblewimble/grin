49556b7735bb26a6a29d5feec2db76528e30e525
Ignotus Peverell <igno.peverell@protonmail.com>
2018-12-21 21:48:09 +0000

    Do not log the phrase in case of error

--- wallet/src/command.rs
+++ wallet/src/command.rs
@@ -88,9 +88,8 @@ pub fn recover(config: &WalletConfig, args: RecoverArgs) -> Result<(), Error> {
 		);
 		if let Err(e) = res {
 			error!(
-				"Error recovering seed with list '{}' - {}",
-				&args.recovery_phrase.as_ref().unwrap(),
-				e
+				"Error recovering seed - {}",
+				e,
 			);
 			return Err(e);
 		}
