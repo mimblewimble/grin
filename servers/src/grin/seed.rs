21deed3ba3d1df97c5b456da1dbc61a0cf6aee2b
Quentin Le Sceller <q.lesceller@gmail.com>
2019-01-06 15:05:57 -0500

    Add Quentin Le Sceller DNS Seed (#2302)

--- servers/src/grin/seed.rs
+++ servers/src/grin/seed.rs
@@ -32,14 +32,16 @@ use crate::util::{Mutex, StopState};
 
 // DNS Seeds with contact email associated
 const MAINNET_DNS_SEEDS: &'static [&'static str] = &[
-	"mainnet.seed.grin-tech.org", // igno.peverell@protonmail.com
-	"mainnet.seed.grin.icu",      // gary.peverell@protonmail.com
-	"mainnet.seed.713.mw",        // jasper@713.mw
+	"mainnet.seed.grin-tech.org",      // igno.peverell@protonmail.com
+	"mainnet.seed.grin.icu",           // gary.peverell@protonmail.com
+	"mainnet.seed.713.mw",             // jasper@713.mw
+	"mainnet.seed.grin.lesceller.com", // q.lesceller@gmail.com
 ];
 const FLOONET_DNS_SEEDS: &'static [&'static str] = &[
-	"floonet.seed.grin-tech.org", // igno.peverell@protonmail.com
-	"floonet.seed.grin.icu",      // gary.peverell@protonmail.com
-	"floonet.seed.713.mw",        // jasper@713.mw
+	"floonet.seed.grin-tech.org",      // igno.peverell@protonmail.com
+	"floonet.seed.grin.icu",           // gary.peverell@protonmail.com
+	"floonet.seed.713.mw",             // jasper@713.mw
+	"floonet.seed.grin.lesceller.com", // q.lesceller@gmail.com
 ];
 
 pub fn connect_and_monitor(
