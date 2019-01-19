0fadc7fc5e371ffa62a2a1eee18d2c9cd154482d
Can Selcik <selcik.can@gmail.com>
2019-01-18 17:14:44 -0800

    Print a clear error message for failure to create db_root directory

--- store/src/lmdb.rs
+++ store/src/lmdb.rs
@@ -66,7 +66,8 @@ pub fn new_env(path: String) -> lmdb::Environment {
 /// Create a new LMDB env under the provided directory with the provided name.
 pub fn new_named_env(path: String, name: String) -> lmdb::Environment {
 	let full_path = [path, name].join("/");
-	fs::create_dir_all(&full_path).unwrap();
+	fs::create_dir_all(&full_path)
+		.expect("Unable to create directory 'db_root' to store chain_data");
 
 	let mut env_builder = lmdb::EnvBuilder::new().unwrap();
 	env_builder.set_maxdbs(8).unwrap();
