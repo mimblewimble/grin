fa7e46d4b2e9bf13e7cde490878f8fc992aba813
Antioch Peverell <apeverell@protonmail.com>
2019-01-30 04:23:23 +0000

    Add comment for Mainnet chain type. (#2483)

--- config/src/comments.rs
+++ config/src/comments.rs
@@ -78,7 +78,8 @@ fn comments() -> HashMap<String, String> {
 #parameters used for mining as well as wallet output coinbase maturity. Can be:
 #AutomatedTesting - For CI builds and instant blockchain creation
 #UserTesting - For regular user testing (cuckoo 16)
-#Floonet - For the long term Floonet test network
+#Floonet - For the long term floonet test network
+#Mainnet - For mainnet
 "
 		.to_string(),
 	);
