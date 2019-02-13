13e73e95342344ac3a79f6e6f8939e8c05a6b1c1
Gary Yu <gairy.yu@gmail.com>
2019-02-13 17:50:46 +0800

    rustfmt

--- chain/tests/test_txhashset.rs
+++ chain/tests/test_txhashset.rs
@@ -27,8 +27,8 @@ use std::sync::Arc;
 use crate::chain::store::ChainStore;
 use crate::chain::txhashset;
 use crate::core::core::BlockHeader;
-use grin_core::core::hash::Hashed;
 use crate::util::file;
+use grin_core::core::hash::Hashed;
 
 fn clean_output_dir(dir_name: &str) {
 	let _ = fs::remove_dir_all(dir_name);
@@ -45,21 +45,31 @@ fn test_unexpected_zip() {
 	let head = BlockHeader::default();
 	// First check if everything works out of the box
 	assert!(txhashset::zip_read(db_root.clone(), &head).is_ok());
-	let zip_path = Path::new(&db_root).join(format!("txhashset_snapshot_{}.zip", head.hash().to_string()));
+	let zip_path = Path::new(&db_root).join(format!(
+		"txhashset_snapshot_{}.zip",
+		head.hash().to_string()
+	));
 	let zip_file = File::open(&zip_path).unwrap();
 	assert!(txhashset::zip_write(db_root.clone(), zip_file, &head).is_ok());
 	// Remove temp txhashset dir
-	assert!(fs::remove_dir_all(Path::new(&db_root).join(format!("txhashset_zip_{}", head.hash().to_string()))).is_err());
+	assert!(fs::remove_dir_all(
+		Path::new(&db_root).join(format!("txhashset_zip_{}", head.hash().to_string()))
+	)
+	.is_err());
 	// Then add strange files in the original txhashset folder
 	write_file(db_root.clone());
 	assert!(txhashset::zip_read(db_root.clone(), &head).is_ok());
 	// Check that the temp dir dos not contains the strange files
-	let txhashset_zip_path = Path::new(&db_root).join(format!("txhashset_zip_{}", head.hash().to_string()));
+	let txhashset_zip_path =
+		Path::new(&db_root).join(format!("txhashset_zip_{}", head.hash().to_string()));
 	assert!(txhashset_contains_expected_files(
 		format!("txhashset_zip_{}", head.hash().to_string()),
 		txhashset_zip_path.clone()
 	));
-	assert!(fs::remove_dir_all(Path::new(&db_root).join(format!("txhashset_zip_{}", head.hash().to_string()))).is_err());
+	assert!(fs::remove_dir_all(
+		Path::new(&db_root).join(format!("txhashset_zip_{}", head.hash().to_string()))
+	)
+	.is_err());
 
 	let zip_file = File::open(zip_path).unwrap();
 	assert!(txhashset::zip_write(db_root.clone(), zip_file, &head).is_ok());
