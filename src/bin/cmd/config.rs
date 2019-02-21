ea4b4fc3897af94ab7792b5fef0a172981eb5382
Yeastplume <yeastplume@gmail.com>
2019-02-21 11:57:45 +0000

    Remove wallet code [1.1.0] (#2593)
    * Bump version to 1.1.0
    
    * remove wallet crate
    
    * wallet extracted + mine_block structs
    
    * remove wallet doc
    
    * remove unnecessary cargo deps
    
    * rustfmt
    
    * remove wallet from travis matrix
    
    * move integration tests into separate crate
    
    * rustfmt
    
    * move integration crate to wallet

--- src/bin/cmd/config.rs
+++ src/bin/cmd/config.rs
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /// Grin configuration file output command
-use crate::config::{config, GlobalConfig, GlobalWalletConfig, GRIN_WALLET_DIR};
+use crate::config::GlobalConfig;
 use crate::core::global;
 use std::env;
 
@@ -42,49 +42,4 @@ pub fn config_command_server(chain_type: &global::ChainTypes, file_name: &str) {
 		"{} file configured and created in current directory",
 		file_name
 	);
-}
-
-/// Create a config file in the current directory
-pub fn config_command_wallet(chain_type: &global::ChainTypes, file_name: &str) {
-	let mut default_config = GlobalWalletConfig::for_chain(chain_type);
-	let current_dir = env::current_dir().unwrap_or_else(|e| {
-		panic!("Error creating config file: {}", e);
-	});
-	let mut config_file_name = current_dir.clone();
-	config_file_name.push(file_name);
-
-	let mut data_dir_name = current_dir.clone();
-	data_dir_name.push(GRIN_WALLET_DIR);
-
-	if config_file_name.exists() && data_dir_name.exists() {
-		panic!(
-			"{} already exists in the target directory. Please remove it first",
-			file_name
-		);
-	}
-
-	// just leave as is if file exists but there's no data dir
-	if config_file_name.exists() {
-		return;
-	}
-
-	default_config.update_paths(&current_dir);
-	default_config
-		.write_to_file(config_file_name.to_str().unwrap())
-		.unwrap_or_else(|e| {
-			panic!("Error creating config file: {}", e);
-		});
-
-	println!(
-		"File {} configured and created",
-		config_file_name.to_str().unwrap(),
-	);
-
-	let mut api_secret_path = current_dir.clone();
-	api_secret_path.push(config::API_SECRET_FILE_NAME);
-	if !api_secret_path.exists() {
-		config::init_api_secret(&api_secret_path).unwrap();
-	} else {
-		config::check_api_secret(&api_secret_path).unwrap();
-	}
 }
