69ad669b65771f7c33cc0842a1312d494a4413a0
antiochp <30642645+antiochp@users.noreply.github.com>
2019-03-11 13:15:54 +0000

    fix rebase onto 1.1.0

--- servers/src/common/types.rs
+++ servers/src/common/types.rs
@@ -27,6 +27,7 @@ use crate::p2p;
 use crate::pool;
 use crate::pool::types::DandelionConfig;
 use crate::store;
+use crate::util::RwLock;
 
 /// Error type wrapping underlying module errors.
 #[derive(Debug)]
