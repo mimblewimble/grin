07d5803deb098e52e8237821a3da8d73384d53d2
John Tromp <john.tromp@gmail.com>
2019-06-11 21:05:32 +0200

    update version in new block when previous no longer valid

--- core/src/core/block.rs
+++ core/src/core/block.rs
@@ -565,6 +565,13 @@ impl Block {
 			vec![],
 		)?;
 
+		let height = prev.height + 1;
+
+		let mut version = prev.version;
+		if !consensus::valid_header_version(height, version) {
+			version = HeaderVersion::new(u16::from(prev.version) + 1);
+		}
+
 		let now = Utc::now().timestamp();
 		let timestamp = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(now, 0), Utc);
 
@@ -573,7 +580,8 @@ impl Block {
 		// Caller must validate the block as necessary.
 		Block {
 			header: BlockHeader {
-				height: prev.height + 1,
+				version,
+				height,
 				timestamp,
 				prev_hash: prev.hash(),
 				total_kernel_offset,
