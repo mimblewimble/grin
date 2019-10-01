2c3128ba9812d00bcae90eaf8d5c10b26512d5fc
hashmap <hashmap@gmail.com>
2019-10-01 13:56:39 +0200

    Fix reading POW for edge bits 59 and higher
    It requires reading more than 8 bytes

--- core/src/pow/types.rs
+++ core/src/pow/types.rs
@@ -391,21 +391,54 @@ impl Proof {
 	}
 }
 
-#[inline(always)]
+fn extract_bits(bits: &Vec<u8>, bit_start: usize, bit_count: usize, read_from: usize) -> u64 {
+	let mut buf: [u8; 8] = [0; 8];
+	// read 8 bytes
+	buf.copy_from_slice(&bits[read_from..read_from + 8]);
+	// convert litl endian bits to u64
+	let mut nonce = u64::from_le_bytes(buf);
+	// shake off high bits (they are part of prev number)
+	nonce <<= 64 - (bit_start - read_from * 8) - bit_count;
+	// shake of low bits (next number if any)
+	nonce >> 64 - bit_count
+}
+
 fn read_number(bits: &Vec<u8>, bit_start: usize, bit_count: usize) -> u64 {
 	if bit_count == 0 {
 		return 0;
 	}
-	let mut buf: [u8; 8] = [0; 8];
-	let mut byte_start = bit_start / 8;
-	if byte_start + 8 > bits.len() {
-		byte_start = bits.len() - 8;
+	// find where the first byte to read starts
+	let mut read_from = bit_start / 8;
+	// move back if we are too close to the end of bits
+	if read_from + 8 > bits.len() {
+		read_from = bits.len() - 8;
 	}
-	buf.copy_from_slice(&bits[byte_start..byte_start + 8]);
-	buf.reverse();
-	let mut nonce = u64::from_be_bytes(buf);
-	nonce = nonce << 64 - (bit_start - byte_start * 8) - bit_count;
-	nonce >> 64 - bit_count
+	// calculate max bit we can read up to (+64 bits from the start)
+	let max_read_pos = (read_from + 8) * 8;
+	// calculate max bit we want to read
+	let max_pos = bit_start + bit_count;
+	// check if we can read it all at once
+	if max_read_pos >= max_pos {
+		extract_bits(bits, bit_start, bit_count, read_from)
+	} else {
+		// we need to read more than 8 bytes, we need to read twice
+		// how many valuable bits we can read now
+		let mut read_size = max_read_pos - bit_start;
+		// read it, it's little endian, so we get the low part of the number
+		let low = extract_bits(bits, bit_start, read_size, read_from);
+
+		// how many bits is left to read
+		read_size = max_pos - max_read_pos;
+		// find the suitable byte to read from
+		read_from = max_read_pos / 8;
+		if read_from + 8 > bits.len() {
+			read_from = bits.len() - 8;
+		}
+		// read the second part
+		let high = extract_bits(bits, max_read_pos, read_size, read_from);
+		// put high bits on the right spot in the number
+		(high << bit_count - read_size) + low
+	}
 }
 
 impl Readable for Proof {
@@ -420,6 +453,9 @@ impl Readable for Proof {
 		let nonce_bits = edge_bits as usize;
 		let bits_len = nonce_bits * global::proofsize();
 		let bytes_len = BitVec::bytes_len(bits_len);
+		if bytes_len < 8 {
+			return Err(ser::Error::CorruptedData);
+		}
 		let bits = reader.read_fixed_bytes(bytes_len)?;
 
 		for n in 0..global::proofsize() {
@@ -477,4 +513,41 @@ impl BitVec {
 	fn set_bit_at(&mut self, pos: usize) {
 		self.bits[pos / 8] |= 1 << (pos % 8) as u8;
 	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::ser::{BinReader, BinWriter, ProtocolVersion};
+	use rand::Rng;
+	use std::io::Cursor;
+
+	#[test]
+	fn test_proof_rw() {
+		for edge_bits in 10..64 {
+			let mut proof = Proof::new(gen_proof(edge_bits as u32));
+			proof.edge_bits = edge_bits;
+			let mut buf = Cursor::new(Vec::new());
+			let mut w = BinWriter::new(&mut buf, ProtocolVersion::local());
+			if let Err(e) = proof.write(&mut w) {
+				panic!("failed to write proof {:?}", e);
+			}
+			buf.set_position(0);
+			let mut r = BinReader::new(&mut buf, ProtocolVersion::local());
+			match Proof::read(&mut r) {
+				Err(e) => panic!("failed to read proof: {:?}", e),
+				Ok(p) => assert_eq!(p, proof),
+			}
+		}
+	}
+
+	fn gen_proof(bits: u32) -> Vec<u64> {
+		let mut rng = rand::thread_rng();
+		let mut v = Vec::with_capacity(42);
+		for _ in 0..42 {
+			v.push(rng.gen_range(u64::pow(2, bits - 1), u64::pow(2, bits)));
+		}
+		v
+	}
+
 }
