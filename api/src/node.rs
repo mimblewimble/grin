011f9d910313a75bbe79ca322afc5929067813f8
Quentin Le Sceller <q.lesceller@gmail.com>
2019-10-09 13:45:25 -0400

    Add doc for node API

--- api/src/node.rs
+++ api/src/node.rs
@@ -51,6 +51,19 @@ pub struct Node {
 }
 
 impl Node {
+	/// Create a new API instance with the chain, transaction pool, peers and `sync_state`. All subsequent
+	/// API calls will operate on this instance of node API.
+	///
+	/// # Arguments
+	/// * `chain` - A non-owning reference of the chain `Weak<Chain>`.
+	/// * `tx_pool` - A non-owning reference of the transaction pool `Weak<pool:TransactionPool>`.
+	/// * `peers` - A non-owning reference of the peers `Weak<p2p::Peers>`.
+	/// * `sync_state` - A non-owning reference of the sync_state `Weak<SyncState>`.
+	///
+	/// # Returns
+	/// * An instance of the Node holding references to the current chain, transaction pool, peers and sync_state.
+	///
+
 	pub fn new(
 		chain: Weak<Chain>,
 		tx_pool: Weak<RwLock<pool::TransactionPool>>,
@@ -65,6 +78,20 @@ impl Node {
 		}
 	}
 
+	/// Gets block header given either a height, a hash or an unspent output commitment. Only one parameters is needed.
+	/// If multiple parameters are provided only the first one in the list is used.
+	///
+	/// # Arguments
+	/// * `height` - block height.
+	/// * `hash` - block hash.
+	/// * `commit` - output commitment.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * A [`BlockHeaderPrintable`](../grin_api/types/struct.BlockHeaderPrintable.html)
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
+
 	pub fn get_header(
 		&self,
 		height: Option<u64>,
@@ -77,6 +104,20 @@ impl Node {
 		let hash = header_handler.parse_inputs(height, hash, commit)?;
 		header_handler.get_header_v2(&hash)
 	}
+
+	/// Gets block details given either a height, a hash or an unspent output commitment. Only one parameters is needed.
+	/// If multiple parameters are provided only the first one in the list is used.
+	///
+	/// # Arguments
+	/// * `height` - block height.
+	/// * `hash` - block hash.
+	/// * `commit` - output commitment.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * A [`BlockPrintable`](../grin_api/types/struct.BlockPrintable.html)
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
 
 	pub fn get_block(
 		&self,
@@ -91,6 +132,14 @@ impl Node {
 		block_handler.get_block(&hash, true, true)
 	}
 
+	/// Returns various information about the node, the network and the current sync status.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * A [`Status`](../grin_api/types/struct.Status.html)
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
+
 	pub fn get_status(&self) -> Result<Status, Error> {
 		let status_handler = StatusHandler {
 			chain: self.chain.clone(),
@@ -100,6 +149,14 @@ impl Node {
 		status_handler.get_status()
 	}
 
+	/// Returns the node version and block header version (used by grin-wallet).
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * A [`Version`](../grin_api/types/struct.Version.html)
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
+
 	pub fn get_version(&self) -> Result<Version, Error> {
 		let version_handler = VersionHandler {
 			chain: self.chain.clone(),
@@ -107,12 +164,37 @@ impl Node {
 		version_handler.get_version()
 	}
 
+	/// Returns details about the state of the current fork tip.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * A [`Tip`](../grin_api/types/struct.Tip.html)
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
+
 	pub fn get_tip(&self) -> Result<Tip, Error> {
 		let chain_handler = ChainHandler {
 			chain: self.chain.clone(),
 		};
 		chain_handler.get_tip()
 	}
+
+	/// Returns a [`LocatedTxKernel`](../grin_api/types/struct.LocatedTxKernel.html) based on the kernel excess.
+	/// The `min_height` and `max_height` parameters are both optional.
+	/// If not supplied, `min_height` will be set to 0 and `max_height` will be set to the head of the chain.
+	/// The method will start at the block height `max_height` and traverse the kernel MMR backwards,
+	/// until either the kernel is found or `min_height` is reached.
+	///
+	/// # Arguments
+	/// * `excess` - kernel excess to look for.
+	/// * `min_height` - minimum height to stop the lookup.
+	/// * `max_height` - maximum height to start the lookup.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * A [`LocatedTxKernel`](../grin_api/types/struct.LocatedTxKernel.html)
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
 
 	pub fn get_kernel(
 		&self,
@@ -125,6 +207,22 @@ impl Node {
 		};
 		kernel_handler.get_kernel_v2(excess, min_height, max_height)
 	}
+
+	/// Retrieves details about specifics outputs. Supports retrieval of multiple outputs in a single request.
+	/// Support retrieval by both commitment string and block height.
+	///
+	/// # Arguments
+	/// * `commits` - a vector of unspent output commitments.
+	/// * `start_height` - start height to start the lookup.
+	/// * `end_height` - end height to stop the lookup.
+	/// * `include_proof` - whether or not to include the range proof in the response.
+	/// * `include_merkle_proof` - whether or not to include the merkle proof in the response.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * An [`OutputPrintable`](../grin_api/types/struct.OutputPrintable.html)
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
 
 	pub fn get_outputs(
 		&self,
@@ -146,6 +244,19 @@ impl Node {
 		)
 	}
 
+	/// UTXO traversal. Retrieves last utxos since a `start_index` until a `max`.
+	///
+	/// # Arguments
+	/// * `start_index` - start index in the MMR.
+	/// * `max` - max index in the MMR.
+	/// * `include_proof` - whether or not to include the range proof in the response.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * An [`OutputListing`](../grin_api/types/struct.OutputListing.html)
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
+
 	pub fn get_unspent_outputs(
 		&self,
 		start_index: u64,
@@ -158,12 +269,28 @@ impl Node {
 		output_handler.get_unspent_outputs(start_index, max, include_proof)
 	}
 
+	/// Trigger a validation of the chain state.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * `Ok(())` if the validation was done successfully
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
+
 	pub fn validate_chain(&self) -> Result<(), Error> {
 		let chain_validation_handler = ChainValidationHandler {
 			chain: self.chain.clone(),
 		};
 		chain_validation_handler.validate_chain()
 	}
+
+	/// Trigger a compaction of the chain state to regain storage space.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * `Ok(())` if the compaction was done successfully
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
 
 	pub fn compact_chain(&self) -> Result<(), Error> {
 		let chain_compact_handler = ChainCompactHandler {
@@ -172,12 +299,32 @@ impl Node {
 		chain_compact_handler.compact_chain()
 	}
 
+	/// Retrieves information about stored peers.
+	/// If `None` is provided, will list all stored peers.
+	///
+	/// # Arguments
+	/// * `addr` - a SocketAddr missing link.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * A vector of [`PeerData`](../grin_api/types/struct.PeerData.html)
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
+
 	pub fn get_peers(&self, addr: Option<SocketAddr>) -> Result<Vec<PeerData>, Error> {
 		let peer_handler = PeerHandler {
 			peers: self.peers.clone(),
 		};
 		peer_handler.get_peers(addr)
 	}
+
+	/// Retrieves a list of all connected peers.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * A vector of [`PeerInfoDisplay`](../grin_api/types/struct.PeerInfoDisplay.html)
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
 
 	pub fn get_connected_peers(&self) -> Result<Vec<PeerInfoDisplay>, Error> {
 		let peers_connected_handler = PeersConnectedHandler {
@@ -186,12 +333,34 @@ impl Node {
 		peers_connected_handler.get_connected_peers()
 	}
 
+	/// Bans a specific peer.
+	///
+	/// # Arguments
+	/// * `addr` - a SocketAddr missing link.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * `Ok(())` if the path was correctly set
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
+
 	pub fn ban_peer(&self, addr: SocketAddr) -> Result<(), Error> {
 		let peer_handler = PeerHandler {
 			peers: self.peers.clone(),
 		};
 		peer_handler.ban_peer(addr)
 	}
+
+	/// Unbans a specific peer.
+	///
+	/// # Arguments
+	/// * `addr` - a SocketAddr missing link.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * `Ok(())` if the unban was done successfully
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
 
 	pub fn unban_peer(&self, addr: SocketAddr) -> Result<(), Error> {
 		let peer_handler = PeerHandler {
@@ -200,6 +369,14 @@ impl Node {
 		peer_handler.unban_peer(addr)
 	}
 
+	/// Returns the number of transaction in the transaction pool.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * `usize`
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
+
 	pub fn get_pool_size(&self) -> Result<usize, Error> {
 		let pool_handler = PoolHandler {
 			tx_pool: self.tx_pool.clone(),
@@ -207,18 +384,48 @@ impl Node {
 		pool_handler.get_pool_size()
 	}
 
+	/// Returns the number of transaction in the stem transaction pool.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * `usize`
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
+
 	pub fn get_stempool_size(&self) -> Result<usize, Error> {
 		let pool_handler = PoolHandler {
 			tx_pool: self.tx_pool.clone(),
 		};
 		pool_handler.get_stempool_size()
 	}
+
+	/// Returns the unconfirmed transactions in the transaction pool.
+	/// Will not return transactions in the stempool.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * A vector of [`PoolEntry`](../grin_api/types/struct.PoolEntry.html)
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
+
 	pub fn get_unconfirmed_transactions(&self) -> Result<Vec<PoolEntry>, Error> {
 		let pool_handler = PoolHandler {
 			tx_pool: self.tx_pool.clone(),
 		};
 		pool_handler.get_unconfirmed_transactions()
 	}
+
+	/// Push new transaction to our local transaction pool.
+	///
+	/// # Arguments
+	/// * `tx` - a Transaction missing link.
+	/// * `fluff` - boolean to bypass Dandelion relay.
+	///
+	/// # Returns
+	/// * Result Containing:
+	/// * `Ok(())` if the transaction was pushed successfully
+	/// * or [`Error`](../grin_api/struct.Error.html) if an error is encountered.
+	///
 	pub fn push_transaction(&self, tx: Transaction, fluff: Option<bool>) -> Result<(), Error> {
 		let pool_handler = PoolHandler {
 			tx_pool: self.tx_pool.clone(),
