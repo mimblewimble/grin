c67fc7834a53f2f0b47b80911f2fdab6bd40d5aa
Joseph Goulden <joegoulden@protonmail.com>
2019-11-18 20:31:21 +0000

    tests: add mod for peer tests

--- p2p/src/peer.rs
+++ p2p/src/peer.rs
@@ -16,7 +16,7 @@ use crate::util::{Mutex, RwLock};
 use std::fmt;
 use std::fs::File;
 use std::io::Read;
-use std::net::{IpAddr, Ipv4Addr, Shutdown, SocketAddr, TcpStream};
+use std::net::{Shutdown, TcpStream};
 use std::path::PathBuf;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
@@ -618,45 +618,51 @@ impl NetAdapter for TrackingAdapter {
 	}
 }
 
-#[test]
-fn test_peer_is_allowed_by_default() {
-	let config = P2PConfig::default();
-	let peer = PeerAddr(SocketAddr::new(
-		IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-		3414,
-	));
-	assert!(!Peer::is_denied(&config, peer));
-}
+#[cfg(test)]
+mod test {
+	use crate::{P2PConfig, Peer, PeerAddr};
+	use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-#[test]
-fn test_peer_is_denied() {
-	let mut config = P2PConfig::default();
-	config.peers_deny = Some(vec!["127.0.0.1:3414".to_string()]);
-	let peer = PeerAddr(SocketAddr::new(
-		IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-		3414,
-	));
-	assert!(Peer::is_denied(&config, peer));
-}
+	#[test]
+	fn test_peer_is_allowed_by_default() {
+		let config = P2PConfig::default();
+		let peer = PeerAddr(SocketAddr::new(
+			IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+			3414,
+		));
+		assert!(!Peer::is_denied(&config, peer));
+	}
 
-#[test]
-fn test_peer_with_different_port_is_not_denied() {
-	let mut config = P2PConfig::default();
-	config.peers_deny = Some(vec!["127.0.0.1:3414".to_string()]);
-	let peer = PeerAddr(SocketAddr::new(
-		IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-		3415,
-	));
-	assert!(!Peer::is_denied(&config, peer));
-}
+	#[test]
+	fn test_peer_is_denied() {
+		let mut config = P2PConfig::default();
+		config.peers_deny = Some(vec!["127.0.0.1:3414".to_string()]);
+		let peer = PeerAddr(SocketAddr::new(
+			IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+			3414,
+		));
+		assert!(Peer::is_denied(&config, peer));
+	}
 
-#[test]
-fn test_peer_is_allowed() {
-	let mut config = P2PConfig::default();
-	config.peers_allow = Some(vec!["127.0.0.1:3414".to_string()]);
-	let peer = PeerAddr(SocketAddr::new(
-		IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-		3414,
-	));
-	assert!(!Peer::is_denied(&config, peer));
+	#[test]
+	fn test_peer_with_different_port_is_not_denied() {
+		let mut config = P2PConfig::default();
+		config.peers_deny = Some(vec!["127.0.0.1:3414".to_string()]);
+		let peer = PeerAddr(SocketAddr::new(
+			IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+			3415,
+		));
+		assert!(!Peer::is_denied(&config, peer));
+	}
+
+	#[test]
+	fn test_peer_is_allowed() {
+		let mut config = P2PConfig::default();
+		config.peers_allow = Some(vec!["127.0.0.1:3414".to_string()]);
+		let peer = PeerAddr(SocketAddr::new(
+			IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+			3414,
+		));
+		assert!(!Peer::is_denied(&config, peer));
+	}
 }
