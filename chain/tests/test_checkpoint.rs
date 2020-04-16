e3fc616d32c6af50eeb2e3626b6ffe3dc666592f
antiochp <30642645+antiochp@users.noreply.github.com>
2020-04-16 17:54:47 +0100

    cleanup

--- chain/tests/test_checkpoint.rs
+++ chain/tests/test_checkpoint.rs
@@ -17,7 +17,6 @@ use self::chain_test_helper::{clean_output_dir, init_chain, mine_chain};
 use chain::Tip;
 use grin_chain as chain;
 use grin_core::core::hash::Hashed;
-use grin_core::genesis;
 use grin_util as util;
 
 #[test]
@@ -29,6 +28,9 @@ fn init_from_checkpoint() {
 	// mine some blocks
 	// assert the checkpoint is *previous* block based on chain head
 	let chain = mine_chain(chain_dir, 3);
+	let genesis = chain
+		.get_block(&chain.get_header_by_height(0).unwrap().hash())
+		.unwrap();
 	let head = chain.head().unwrap();
 	let checkpoint = chain.txhashset().read().last_checkpoint().unwrap();
 	assert_eq!(head.prev_block_h, checkpoint.hash());
@@ -36,25 +38,27 @@ fn init_from_checkpoint() {
 
 	// re-init chain from disk
 	// using the checkpoint written previously
-	let chain = init_chain(chain_dir, genesis::genesis_dev());
+	let chain = init_chain(chain_dir, genesis.clone());
 	let head = chain.head().unwrap();
 	assert_eq!(head.prev_block_h, checkpoint.hash());
 	assert_eq!(head_orig, head);
 
 	// reset chain head to earlier state and forget about "latest" block
 	// chain head now matches checkpoint
-	let store = chain.store();
-	let batch = store.batch().unwrap();
-	let latest_block = batch.get_block(&head.last_block_h).unwrap();
-	batch
-		.save_body_head(&Tip::from_header(&checkpoint))
-		.unwrap();
-	batch.delete_block(&latest_block.hash()).unwrap();
-	batch.commit().unwrap();
+	let latest_block = chain.get_block(&head.last_block_h).unwrap();
+	{
+		let store = chain.store();
+		let batch = store.batch().unwrap();
+		batch
+			.save_body_head(&Tip::from_header(&checkpoint))
+			.unwrap();
+		batch.delete_block(&latest_block.hash()).unwrap();
+		batch.commit().unwrap();
+	}
 
 	// re-init chain from disk
 	// assert the chain head corresponds to the checkpoint itself
-	let chain = init_chain(chain_dir, genesis::genesis_dev());
+	let chain = init_chain(chain_dir, genesis.clone());
 	let head = chain.head().unwrap();
 	assert_eq!(head.last_block_h, checkpoint.hash());
 
