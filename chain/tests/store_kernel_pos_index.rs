c830daaa2c1ca12c0efb2a72b302a6345c0e6941
antiochp <30642645+antiochp@users.noreply.github.com>
2020-05-16 20:48:23 +0100

    test coverage for rewindable list index

--- chain/tests/store_kernel_pos_index.rs
+++ chain/tests/store_kernel_pos_index.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::chain::linked_list::{self, ListIndex, ListWrapper};
+use crate::chain::linked_list::{self, ListIndex, ListWrapper, RewindableListIndex};
 use crate::chain::store::{self, ChainStore};
 use crate::chain::types::CommitPos;
 use crate::core::core::OutputFeatures;
@@ -278,6 +278,8 @@ fn test_store_kernel_idx_pop_back() {
 
 	assert_eq!(index.peek_pos(&batch, commit), Ok(None));
 	assert_eq!(index.get_list(&batch, commit), Ok(None));
+
+	clean_output_dir(chain_dir);
 }
 
 #[test]
@@ -287,5 +289,59 @@ fn test_store_kernel_idx_rewind() {
 	let chain_dir = ".grin_idx_3";
 	clean_output_dir(chain_dir);
 
-	panic!("to be continued");
+	let commit = Commitment::from_vec(vec![]);
+
+	let store = ChainStore::new(chain_dir).unwrap();
+	let batch = store.batch().unwrap();
+	let index = store::coinbase_kernel_index();
+
+	assert_eq!(
+		index.push_pos(&batch, commit, CommitPos { pos: 1, height: 1 }),
+		Ok(()),
+	);
+
+	assert_eq!(
+		index.push_pos(&batch, commit, CommitPos { pos: 2, height: 2 }),
+		Ok(()),
+	);
+
+	assert_eq!(
+		index.push_pos(&batch, commit, CommitPos { pos: 3, height: 3 }),
+		Ok(()),
+	);
+
+	assert_eq!(
+		index.get_list(&batch, commit),
+		Ok(Some(ListWrapper::Multi { head: 3, tail: 1 })),
+	);
+
+	assert_eq!(index.rewind(&batch, commit, 1), Ok(()),);
+
+	assert_eq!(
+		index.get_list(&batch, commit),
+		Ok(Some(ListWrapper::Single {
+			pos: CommitPos { pos: 1, height: 1 }
+		})),
+	);
+
+	// Check we can safely noop rewind.
+	assert_eq!(index.rewind(&batch, commit, 2), Ok(()),);
+
+	assert_eq!(
+		index.get_list(&batch, commit),
+		Ok(Some(ListWrapper::Single {
+			pos: CommitPos { pos: 1, height: 1 }
+		})),
+	);
+
+	assert_eq!(index.rewind(&batch, commit, 1), Ok(()),);
+
+	assert_eq!(
+		index.get_list(&batch, commit),
+		Ok(Some(ListWrapper::Single {
+			pos: CommitPos { pos: 1, height: 1 }
+		})),
+	);
+
+	clean_output_dir(chain_dir);
 }
