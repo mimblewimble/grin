20fcf0c0b5140630f57db7dda2836f4b27270fc0
antiochp <30642645+antiochp@users.noreply.github.com>
2020-05-28 15:02:29 +0100

    PR feedback

--- chain/tests/mine_nrd_kernel.rs
+++ chain/tests/mine_nrd_kernel.rs
@@ -103,3 +103,49 @@ fn mine_block_with_nrd_kernel_and_nrd_feature_enabled() {
 
 	clean_output_dir(chain_dir);
 }
+
+#[test]
+fn mine_invalid_block_with_nrd_kernel_and_nrd_feature_enabled_before_hf() {
+	global::set_local_chain_type(global::ChainTypes::AutomatedTesting);
+	global::set_local_nrd_enabled(true);
+
+	util::init_test_logger();
+
+	let chain_dir = ".grin.invalid_nrd_kernel";
+	clean_output_dir(chain_dir);
+
+	let keychain = ExtKeychain::from_random_seed(false).unwrap();
+	let pb = ProofBuilder::new(&keychain);
+	let genesis = genesis_block(&keychain);
+	let chain = init_chain(chain_dir, genesis.clone());
+
+	for n in 1..8 {
+		let key_id = ExtKeychainPath::new(1, n, 0, 0, 0).to_identifier();
+		let block = build_block(&chain, &keychain, &key_id, vec![]);
+		chain.process_block(block, Options::MINE).unwrap();
+	}
+
+	assert_eq!(chain.head().unwrap().height, 7);
+
+	let key_id1 = ExtKeychainPath::new(1, 1, 0, 0, 0).to_identifier();
+	let key_id2 = ExtKeychainPath::new(1, 2, 0, 0, 0).to_identifier();
+	let tx = build::transaction(
+		KernelFeatures::NoRecentDuplicate {
+			fee: 20000,
+			relative_height: NRDRelativeHeight::new(1440).unwrap(),
+		},
+		vec![
+			build::coinbase_input(consensus::REWARD, key_id1.clone()),
+			build::output(consensus::REWARD - 20000, key_id2.clone()),
+		],
+		&keychain,
+		&pb,
+	)
+	.unwrap();
+
+	let key_id8 = ExtKeychainPath::new(1, 8, 0, 0, 0).to_identifier();
+	let block = build_block(&chain, &keychain, &key_id8, vec![tx]);
+	let res = chain.process_block(block, Options::MINE);
+	assert!(res.is_err());
+	clean_output_dir(chain_dir);
+}
