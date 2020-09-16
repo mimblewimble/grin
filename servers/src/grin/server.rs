dc47beda78fe982536117a21f107485f439c82d1
Blade Doyle <bdoyle@lsd25.net>
2020-09-16 10:03:42 -0700

    cleanup mining stats (#3439)
    * cleanup mining stats; make edge_bits dynamic (last size submitted), dont show bogus stats when no miners are connected
    
    Co-authored-by: Quentin Le Sceller <q.lesceller@gmail.com>

--- servers/src/grin/server.rs
+++ servers/src/grin/server.rs
@@ -361,7 +361,6 @@ impl Server {
 
 	/// Start a minimal "stratum" mining service on a separate thread
 	pub fn start_stratum_server(&self, config: StratumServerConfig) {
-		let edge_bits = global::min_edge_bits();
 		let proof_size = global::proofsize();
 		let sync_state = self.sync_state.clone();
 
@@ -375,7 +374,7 @@ impl Server {
 		let _ = thread::Builder::new()
 			.name("stratum_server".to_string())
 			.spawn(move || {
-				stratum_server.run_loop(edge_bits as u32, proof_size, sync_state);
+				stratum_server.run_loop(proof_size, sync_state);
 			});
 	}
 
