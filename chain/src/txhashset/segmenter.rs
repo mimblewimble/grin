2227cd3315a9e5e7265d7b87977e766a884a2bcc
antiochp <30642645+antiochp@users.noreply.github.com>
2020-10-29 12:23:39 +0000

    placeholder segment impl

--- chain/src/txhashset/segmenter.rs
+++ chain/src/txhashset/segmenter.rs
@@ -14,13 +14,15 @@
 
 //! Generation of the various necessary segments requested during PIBD.
 
-use std::sync::Arc;
+use std::{marker, sync::Arc};
 
 use crate::core::core::hash::Hash;
-use crate::core::core::pmmr::ReadablePMMR;
-use crate::core::core::BlockHeader;
+use crate::core::core::pmmr::{Backend, ReadablePMMR, ReadonlyPMMR};
+use crate::core::core::{BlockHeader, OutputIdentifier, TxKernel};
+use crate::core::ser::{PMMRable, Readable, Writeable};
 use crate::error::{Error, ErrorKind};
-use crate::txhashset::{self, PMMRHandle, TxHashSet};
+use crate::txhashset::{self, BitmapChunk, PMMRHandle, TxHashSet};
+use crate::util::secp::pedersen::RangeProof;
 use crate::util::RwLock;
 
 /// TODO - Replace this with SegmentIdentifier from segment PR.
@@ -29,7 +31,35 @@ pub struct SegmentIdentifier {
 	idx: u64,
 }
 
-/// Segmenter for generating PIBS segments.
+/// TODO - Replace this the real Segment
+pub struct Segment<T> {
+	id: SegmentIdentifier,
+	_marker: marker::PhantomData<T>,
+}
+
+/// TODO - Replace this the real Segment
+impl<T> Segment<T>
+where
+	T: std::fmt::Debug + Readable + Writeable,
+{
+	/// Generate a segment from a PMMR
+	pub fn from_pmmr<U, B>(
+		segment_id: SegmentIdentifier,
+		pmmr: &ReadonlyPMMR<'_, U, B>,
+		prunable: bool,
+	) -> Result<Self, Error>
+	where
+		U: PMMRable<E = T>,
+		B: Backend<U>,
+	{
+		Ok(Segment {
+			id: segment_id,
+			_marker: marker::PhantomData,
+		})
+	}
+}
+
+/// Segmenter for generating PIBD segments.
 pub struct Segmenter {
 	header_pmmr: Arc<RwLock<PMMRHandle<BlockHeader>>>,
 	txhashset: Arc<RwLock<TxHashSet>>,
@@ -52,25 +82,23 @@ impl Segmenter {
 
 	/// Create a kernel segment.
 	/// We use a lightweight "rewindable kernel view" here as we do not need to worry about pruning.
-	pub fn kernel_segment(&self, _id: SegmentIdentifier) -> Result<&str, Error> {
+	pub fn kernel_segment(&self, id: SegmentIdentifier) -> Result<Segment<TxKernel>, Error> {
 		let txhashset = self.txhashset.read();
 		txhashset::rewindable_kernel_view(&txhashset, |view, _| {
 			// This rewind is fast as we take advantage of our "rewindable kernel view".
 			view.rewind(&self.header)?;
-
-			// TODO - Generate segment from our rewound kernel view.
 			let pmmr = view.readonly_pmmr();
-			// let segment = Segment::from_pmmr(id, &pmmr, false)?;
-
-			Ok(())
-		})?;
-
-		Ok("this will return a segment")
+			let segment = Segment::from_pmmr(id, &pmmr, false)?;
+			Ok(segment)
+		})
 	}
 
+	/// Create a utxo bitmap segment.
 	/// Note: we need to rewind both the bitmap and the output MMR as we need both roots here.
-	/// TODO - Return a segment and the corresponding output root.
-	pub fn bitmap_segment(&self, _id: SegmentIdentifier) -> Result<(&str, Hash), Error> {
+	pub fn bitmap_segment(
+		&self,
+		id: SegmentIdentifier,
+	) -> Result<(Segment<BitmapChunk>, Hash), Error> {
 		let mut header_pmmr = self.header_pmmr.write();
 		let mut txhashset = self.txhashset.write();
 		txhashset::extending_readonly(&mut header_pmmr, &mut txhashset, |ext, batch| {
@@ -82,25 +110,24 @@ impl Segmenter {
 			// to allow for fast subsequent reads?
 			extension.rewind(&self.header, batch)?;
 
-			// TODO - Generate segment from our rewound extension.
 			let bitmap_pmmr = extension.bitmap_readonly_pmmr();
-			// let segment = Segment::from_pmmr(id, &bitmap_pmmr, true)?;
+			let segment = Segment::from_pmmr(id, &bitmap_pmmr, true)?;
 
 			let output_pmmr = extension.output_readonly_pmmr();
 			let output_root = output_pmmr
 				.root()
 				.map_err(|_| ErrorKind::TxHashSetErr("failed to get output root".into()))?;
 
-			Ok((
-				"this will return a bitmap segment and corresponding output root",
-				output_root,
-			))
+			Ok((segment, output_root))
 		})
 	}
 
+	/// Create an output segment.
 	/// Note: we need to rewind both the bitmap and the output MMR as we need both roots here.
-	/// TODO - Return a segment and the corresponding bitmap root.
-	pub fn output_segment(&self, _id: SegmentIdentifier) -> Result<(&str, Hash), Error> {
+	pub fn output_segment(
+		&self,
+		id: SegmentIdentifier,
+	) -> Result<(Segment<OutputIdentifier>, Hash), Error> {
 		let mut header_pmmr = self.header_pmmr.write();
 		let mut txhashset = self.txhashset.write();
 		txhashset::extending_readonly(&mut header_pmmr, &mut txhashset, |ext, batch| {
@@ -112,24 +139,20 @@ impl Segmenter {
 			// Then we can directly init a ReadonlyPMMR with the correct sizes for subsequent reads.
 			extension.rewind(&self.header, batch)?;
 
-			// TODO - Generate segment from our rewound extension.
 			let output_pmmr = extension.output_readonly_pmmr();
-			// let segment = Segment::from_pmmr(id, &output_pmmr, true)?;
+			let segment = Segment::from_pmmr(id, &output_pmmr, true)?;
 
 			let bitmap_pmmr = extension.bitmap_readonly_pmmr();
 			let bitmap_root = bitmap_pmmr
 				.root()
 				.map_err(|_| ErrorKind::TxHashSetErr("failed to get bitmap root".into()))?;
 
-			Ok((
-				"this will return an output segment and corresponding bitmap root",
-				bitmap_root,
-			))
+			Ok((segment, bitmap_root))
 		})
 	}
 
 	/// Create a rangeproof segment.
-	pub fn rangeproof_segment(&self, _id: SegmentIdentifier) -> Result<&str, Error> {
+	pub fn rangeproof_segment(&self, id: SegmentIdentifier) -> Result<Segment<RangeProof>, Error> {
 		let mut header_pmmr = self.header_pmmr.write();
 		let mut txhashset = self.txhashset.write();
 		txhashset::extending_readonly(&mut header_pmmr, &mut txhashset, |ext, batch| {
@@ -141,11 +164,9 @@ impl Segmenter {
 			// Then we can directly init a ReadonlyPMMR with the correct sizes for subsequent reads.
 			extension.rewind(&self.header, batch)?;
 
-			// TODO - Generate segment from our rewound extension.
 			let pmmr = extension.rproof_readonly_pmmr();
-			// let segment = Segment::from_pmmr(id, &pmmr, false)?;
-
-			Ok("this will return a segment")
+			let segment = Segment::from_pmmr(id, &pmmr, false)?;
+			Ok(segment)
 		})
 	}
 }
