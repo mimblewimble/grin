185de7099cf19dd4adc0eac632cff1a2571a616f
Quentin <quentin@Valentins-MacBook-Pro.local>
2020-11-25 13:57:58 -0500

    Fix missing path in foreign_api_secret

--- config/src/config.rs
+++ config/src/config.rs
@@ -249,10 +249,17 @@ impl GlobalConfig {
 		let mut chain_path = grin_home.clone();
 		chain_path.push(GRIN_CHAIN_DIR);
 		self.members.as_mut().unwrap().server.db_root = chain_path.to_str().unwrap().to_owned();
-		let mut secret_path = grin_home.clone();
-		secret_path.push(API_SECRET_FILE_NAME);
+		let mut api_secret_path = grin_home.clone();
+		api_secret_path.push(API_SECRET_FILE_NAME);
 		self.members.as_mut().unwrap().server.api_secret_path =
-			Some(secret_path.to_str().unwrap().to_owned());
+			Some(api_secret_path.to_str().unwrap().to_owned());
+		let mut foreign_api_secret_path = grin_home.clone();
+		foreign_api_secret_path.push(FOREIGN_API_SECRET_FILE_NAME);
+		self.members
+			.as_mut()
+			.unwrap()
+			.server
+			.foreign_api_secret_path = Some(foreign_api_secret_path.to_str().unwrap().to_owned());
 		let mut log_path = grin_home.clone();
 		log_path.push(SERVER_LOG_FILE_NAME);
 		self.members
