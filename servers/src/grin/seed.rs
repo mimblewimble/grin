020658703e0d5c554bc6d7952abed70e317b985f
antiochp <30642645+antiochp@users.noreply.github.com>
2020-12-11 22:57:57 +0000

    prefer outbound peers when syncing (consistently) but use inbound peer for header and body sync if necessary sync state from inbound peer if no outbound peers to sync from

--- servers/src/grin/seed.rs
+++ servers/src/grin/seed.rs
@@ -26,6 +26,7 @@ use std::sync::{mpsc, Arc};
 use std::{cmp, str, thread, time};
 
 use crate::core::global;
+use crate::core::pow::Difficulty;
 use crate::p2p;
 use crate::p2p::types::PeerAddr;
 use crate::p2p::ChainAdapter;
@@ -172,15 +173,10 @@ fn monitor_peers(
 		total_count += 1;
 	}
 
-	let peers_count = peers.iter().connected().count();
-
-	let max_diff = peers.max_peer_difficulty();
-	let most_work_count = peers
-		.iter()
-		.outbound()
-		.with_difficulty(|x| x >= max_diff)
-		.connected()
-		.count();
+	let peers_iter = || peers.iter().connected();
+	let peers_count = peers_iter().count();
+	let max_diff = peers_iter().max_difficulty().unwrap_or(Difficulty::zero());
+	let most_work_count = peers_iter().with_difficulty(|x| x >= max_diff).count();
 
 	debug!(
 		"monitor_peers: on {}:{}, {} connected ({} most_work). \
--- servers/src/grin/sync/state_sync.rs
+++ servers/src/grin/sync/state_sync.rs
@@ -19,6 +19,7 @@ use std::sync::Arc;
 use crate::chain::{self, SyncState, SyncStatus};
 use crate::core::core::hash::Hashed;
 use crate::core::global;
+use crate::core::pow::Difficulty;
 use crate::p2p::{self, Capabilities, Peer};
 
 /// Fast sync has 3 "states":
@@ -158,15 +159,23 @@ impl StateSync {
 		let mut txhashset_height = header_head.height.saturating_sub(threshold);
 		txhashset_height = txhashset_height.saturating_sub(txhashset_height % archive_interval);
 
-		let max_diff = self.peers.max_peer_difficulty();
-		let peer = self
-			.peers
-			.iter()
-			.outbound()
-			.with_capabilities(Capabilities::TXHASHSET_HIST)
-			.with_difficulty(|x| x >= max_diff)
-			.connected()
-			.choose_random();
+		let peers_iter = || {
+			self.peers
+				.iter()
+				.with_capabilities(Capabilities::TXHASHSET_HIST)
+				.connected()
+		};
+
+		// Filter peers further based on max difficulty.
+		let max_diff = peers_iter().max_difficulty().unwrap_or(Difficulty::zero());
+		let peers_iter = || peers_iter().with_difficulty(|x| x >= max_diff);
+
+		// Choose a random "most work" peer, preferring outbound if at all possible.
+		let peer = peers_iter().outbound().choose_random().or_else(|| {
+			warn!("no suitable outbound peer for state sync, considering inbound");
+			peers_iter().inbound().choose_random()
+		});
+
 		if let Some(peer) = peer {
 			// ask for txhashset at state_sync_threshold
 			let mut txhashset_head = self
