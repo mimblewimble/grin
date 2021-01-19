4fc8d57b37cf2e8840ee73c009429ec437f63412
Alexandre Bury <alexandre.bury@gmail.com>
2021-01-19 10:58:29 -0800

    Update to cursive 0.16

--- src/bin/tui/ui.rs
+++ src/bin/tui/ui.rs
@@ -29,8 +29,7 @@ use cursive::utils::markup::StyledString;
 use cursive::views::{
 	CircularFocus, Dialog, LinearLayout, Panel, SelectView, StackView, TextView, ViewRef,
 };
-use cursive::Cursive;
-use cursive::CursiveExt;
+use cursive::{CursiveRunnable, CursiveRunner};
 use std::sync::mpsc;
 use std::{thread, time};
 
@@ -44,7 +43,7 @@ use grin_core::global;
 use grin_util::logger::LogEntry;
 
 pub struct UI {
-	cursive: Cursive,
+	cursive: CursiveRunner<CursiveRunnable>,
 	ui_rx: mpsc::Receiver<UIMessage>,
 	ui_tx: mpsc::Sender<UIMessage>,
 	controller_tx: mpsc::Sender<ControllerMessage>,
@@ -72,7 +71,7 @@ impl UI {
 		let (ui_tx, ui_rx) = mpsc::channel::<UIMessage>();
 
 		let mut grin_ui = UI {
-			cursive: Cursive::default(),
+			cursive: cursive::default().into_runner(),
 			ui_tx,
 			ui_rx,
 			controller_tx,
