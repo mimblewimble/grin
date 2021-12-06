048af14c5ec2a3e4ef21ae8cd22a8a2705a1b2a5
yeastplume <yeastplume@protonmail.com>
2021-12-06 15:59:58 +0000

    remainder fix for new logic

--- core/src/pow/types.rs
+++ core/src/pow/types.rs
@@ -21,7 +21,7 @@ use rand::{thread_rng, Rng};
 use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 /// Types for a Cuck(at)oo proof of work and its encapsulation as a fully usable
 /// proof of work within a block header.
-use std::cmp::{max, min};
+use std::cmp::{max, min, Ordering};
 use std::ops::{Add, Div, Mul, Sub};
 use std::u64;
 use std::{fmt, iter};
@@ -405,11 +405,20 @@ impl Proof {
 	/// Pack the nonces of the proof to their exact bit size as described above
 	pub fn pack_nonces(&self) -> Vec<u8> {
 		let mut compressed = vec![0u8; Proof::pack_len(self.edge_bits)];
+		pack_bits_old(
+			self.edge_bits,
+			&self.nonces[0..self.nonces.len()],
+			&mut compressed,
+		);
+		println!("PACK OLD: {:?}", compressed);
+
+		let mut compressed = vec![0u8; Proof::pack_len(self.edge_bits)];
 		pack_bits(
 			self.edge_bits,
 			&self.nonces[0..self.nonces.len()],
 			&mut compressed,
 		);
+		println!("PACK NEW: {:?}", compressed);
 		compressed
 	}
 
@@ -439,9 +448,58 @@ fn pack_bits(bit_width: u8, uncompressed: &[u64], mut compressed: &mut [u8]) {
 			remaining = 64 + remaining - bit_width;
 		}
 	}
-	let remainder = compressed.len() % 8;
-	if remainder > 0 {
-		compressed[..remainder].copy_from_slice(&mini_buffer.to_le_bytes()[..remainder]);
+	let mut remainder = compressed.len() % 8;
+	if remainder == 0 {
+		remainder = 8;
+	}
+	if mini_buffer > 0 {
+		compressed[..].copy_from_slice(&mini_buffer.to_le_bytes()[..remainder]);
+	}
+}
+
+/// Pack an array of u64s into `compressed` at the specified bit width. Caller
+/// must ensure `compressed` is the right size
+fn pack_bits_old(bit_width: u8, uncompressed: &[u64], mut compressed: &mut [u8]) {
+	// We will use a `u64` as a mini buffer of 64 bits.
+	// We accumulate bits in it until capacity, at which point we just copy this
+	// mini buffer to compressed.
+	let mut mini_buffer: u64 = 0u64;
+	let mut cursor = 0; //< number of bits written in the mini_buffer.
+	let mut pack_bytes_remaining = compressed.len();
+	for el in uncompressed {
+		let remaining = 64 - cursor;
+		match bit_width.cmp(&remaining) {
+			Ordering::Less => {
+				// Plenty of room remaining in our mini buffer.
+				mini_buffer |= el << cursor;
+				cursor += bit_width;
+			}
+			Ordering::Equal => {
+				mini_buffer |= el << cursor;
+				// We have completed our minibuffer exactly.
+				// Let's write it to `compressed`.
+				compressed[..8].copy_from_slice(&mini_buffer.to_le_bytes());
+				compressed = &mut compressed[8..];
+				pack_bytes_remaining -= 8;
+				mini_buffer = 0u64;
+				cursor = 0;
+			}
+			Ordering::Greater => {
+				mini_buffer |= el << cursor;
+				// We have completed our minibuffer.
+				// Let's write it to `compressed` and set the fresh mini_buffer
+				// with the remaining bits.
+				compressed[..8].copy_from_slice(&mini_buffer.to_le_bytes());
+				compressed = &mut compressed[8..];
+				pack_bytes_remaining -= 8;
+				cursor = bit_width - remaining;
+				mini_buffer = el >> remaining;
+			}
+		}
+	}
+	if pack_bytes_remaining > 0 {
+		compressed[..pack_bytes_remaining]
+			.copy_from_slice(&mini_buffer.to_le_bytes()[..pack_bytes_remaining]);
 	}
 }
 
@@ -533,6 +591,7 @@ mod tests {
 		for edge_bits in 10..63 {
 			let mut proof = Proof::new(gen_proof(edge_bits as u32));
 			proof.edge_bits = edge_bits;
+			println!("EDGE BITS: {}", edge_bits);
 			let mut buf = Cursor::new(Vec::new());
 			let mut w = BinWriter::new(&mut buf, ProtocolVersion::local());
 			if let Err(e) = proof.write(&mut w) {
